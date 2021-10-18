1e9bced5fd93708a0d2099f05f52253b25dc2fab
lahuuki <lahuuki@gmail.com>
2021-10-18 16:05:31 -0400

    Copy in example SPEAQeasy script

---
+++ code/SPEAQeasy/run_pipeline_jhpce.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#$ -l mem_free=40G,h_vmem=40G,h_fsize=800G
+#$ -o ./SPEAQeasy_output.log
+#$ -e ./SPEAQeasy_output.log
+#$ -cwd
+
+#  After running 'install_software.sh', this should point to the directory
+#  where SPEAQeasy was installed, and not say "$PWD"
+ORIG_DIR=/users/lhuuki/SPEAQeasy
+
+module load nextflow
+export _JAVA_OPTIONS="-Xms8g -Xmx10g"
+
+nextflow $ORIG_DIR/main.nf \
+    --sample "paired" \
+    --reference "hg38" \
+    --strand "forward" \
+    --input "test/human/paired/forward" \
+    --output "test_output/" \
+    --annotation "/dcs04/lieber/lcolladotor/annotationFiles_LIBD001/SPEAQeasy/Annotation" \
+    -with-report execution_reports/JHPCE_run.html \
+    -profile jhpce
+
+#  Produces a report for each sample tracing the pipeline steps
+#  performed (can be helpful for debugging).
+#
+#  Note that the reports are generated from the output log produced in the above
+#  section, and so if you rename the log, you must also pass replace the filename
+#  in the bash call below.
+echo "Generating per-sample logs for debugging..."
+bash $ORIG_DIR/scripts/generate_logs.sh $PWD/SPEAQeasy_output.log
