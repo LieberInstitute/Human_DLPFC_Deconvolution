191b9ff210ead64a4a9f8e6a9f9e1da8786de135
lahuuki <lahuuki@gmail.com>
2021-10-20 13:30:29 -0400

    Update strand to 'reverse'

--- code/01_SPEAQeasy/run_pipeline_jhpce.sh
+++ code/01_SPEAQeasy/run_pipeline_jhpce.sh
@@ -14,13 +14,15 @@ export _JAVA_OPTIONS="-Xms8g -Xmx10g"
 nextflow $ORIG_DIR/main.nf \
     --sample "paired" \
     --reference "hg38" \
-    --strand "forward" \
+    --strand "reverse" \
     --input "/dcs04/lieber/lcolladotor/deconvolution_LIBD4030/Human_DLPFC_Deconvolution/raw-data/bulkRNA" \
     --output "/dcs04/lieber/lcolladotor/deconvolution_LIBD4030/Human_DLPFC_Deconvolution/processed-data/01_SPEAQeasy/round1_2021-10-19" \
     --annotation "/dcs04/lieber/lcolladotor/annotationFiles_LIBD001/SPEAQeasy/Annotation" \
     --experiment "Human_DLPFC_Deconvolution" \
+    --strand_mode 'accept' \
     -with-report execution_reports/JHPCE_run.html \
-    -profile jhpce
+    -profile jhpce \
+	-resume
 
 #  Produces a report for each sample tracing the pipeline steps
 #  performed (can be helpful for debugging).
