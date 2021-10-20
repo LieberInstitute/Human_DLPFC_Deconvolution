4a74c97e54aa3aa341b93a309575bbbdd1374d7f
lahuuki <lahuuki@gmail.com>
2021-10-20 13:20:50 -0400

    Add library type to pd and boxplots

--- code/02_quality_control/quality_control_check.R
+++ code/02_quality_control/quality_control_check.R
@@ -2,13 +2,30 @@ library(SummarizedExperiment)
 library(tidyverse)
 library(sessioninfo)
 library(here)
+library(readxl)
 
 #### Load Big Data ####
 load("/dcl01/ajaffe/data/lab/brain_swap/count_data/RNAseq_Collection_postQC_n5761_12dataset_2021-06_geneRSE.Rdata", verbose = TRUE)
 dim(rse_gene)
 # [1] 58037  5761
 
-pd <- as.data.frame(colData(rse_gene))
+bsp2_library_type <- read_excel('/dcl01/lieber/ajaffe/lab/brainseq_phase2/misc/LIBD_PhaseII_HIPPO_RiboZero_sample_list_01_28_2015.xlsx') %>%
+  mutate(RNum = paste0("R", RNum)) %>%
+  select(RNum, library_type = Protocol)
+
+table(bsp2_library_type$library_type)
+# RiboZeroGold  RiboZeroHMR 
+# 47          405 
+
+
+pd <- as.data.frame(colData(rse_gene)) %>%
+  left_join(bsp2_library_type) %>%
+  replace_na(list(library_type = "RiboZeroGold")) %>%
+  mutate(library_type = ifelse(Dataset == "BrainSeq_Phase1", "PolyA", library_type))
+
+table(pd$library_type)
+# PolyA RiboZeroGold  RiboZeroHMR 
+# 727         4630          404 
 
 table(pd$Dataset)
 # Astellas_DG         BrainSeq_Phase1   BrainSeq_Phase2_DLPFC   BrainSeq_Phase2_HIPPO BrainSeq_Phase3_Caudate 
@@ -23,22 +40,22 @@ colnames(pd)
 # [6] "BrNum"             "Dx"                "Age"               "Sex"               "Race"             
 # [11] "numReads"          "numMapped"         "numUnmapped"       "mitoMapped"        "totalMapped"      
 # [16] "overallMapRate"    "concordMapRate"    "mitoRate"          "rRNA_rate"         "totalAssignedGene"
-# [21] "bamFile"           "rna_preSwap_BrNum"
+# [21] "bamFile"           "rna_preSwap_BrNum" "library_type" 
 
 ## Subset and QC data
 qc_variables <- c("numReads", "overallMapRate","mitoRate", "rRNA_rate", "totalAssignedGene")
 
 pd_qc_long <- pd %>%
-  select(SAMPLE_ID, Dataset, all_of(qc_variables)) %>%
-  pivot_longer(!c(SAMPLE_ID, Dataset), names_to = "qc_var") %>%
+  select(SAMPLE_ID, Dataset, library_type, all_of(qc_variables)) %>%
+  pivot_longer(!c(SAMPLE_ID, Dataset, library_type), names_to = "qc_var") %>%
   mutate(`Data Status` = "old")
 
 qc_boxplots <- ggplot(data = pd_qc_long, aes(x = Dataset, y = value)) +
-  geom_boxplot(aes(fill = `Data Status`)) +
+  geom_boxplot(aes(fill = `library_type`)) +
   facet_wrap(~qc_var, scales = "free_y", ncol = 2 ) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        text = element_text(size = 15))
+        text = element_text(size = 17))
 
-ggsave(qc_boxplots, filename = here("plots", "quality_control", "qc_boxplots.png"), width = 10, height = 12)
+ggsave(qc_boxplots, filename = here("plots", "02_quality_control", "qc_boxplots.png"), width = 12, height = 12)
 
   
