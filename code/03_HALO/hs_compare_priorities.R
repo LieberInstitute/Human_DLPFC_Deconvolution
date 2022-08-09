a1260d668404c12d38d600afc470e56bf7abe1cb
Sean Maden <metamaden@users.noreply.github.com>
2022-08-09 10:34:23 -0700

    Update hs_compare_priorities.R
    adds helper functions to compare priority ranks among params
    finds params with non-zero median priority rank diffs among samples

--- code/03_HALO/hs_compare_priorities.R
+++ code/03_HALO/hs_compare_priorities.R
@@ -21,6 +21,21 @@ ht <- get_halo_settings_table(fnv)
 #-----------------
 # helper functions
 #-----------------
+# get the median rank difference for a single parameter
+param_median_rankdiff <- function(pi, ht){
+  usampv <- unique(ht$filename)
+  cond.parami <- ht$parameter_name==pi
+  medv <- sapply(usampv, function(si){
+    cond.sampi <- ht$filename==si; 
+    cond.filt <- cond.sampi & cond.parami
+    pri <- as.numeric(ht[cond.filt,]$priority)
+    # return abs priority difference summaries
+    abs.diffi <- abs(pri-as.numeric(ht[cond.parami & !cond.sampi,]$priority))
+    median(abs.diffi)
+  })
+  return(medv)
+}
+
 # get summary statistics for rank differences
 get_rankdiff_sstat <- function(ht, pi, usampv){
   cond.parami <- ht$parameter_name==pi
@@ -38,29 +53,41 @@ get_rankdiff_sstat <- function(ht, pi, usampv){
   return(dt) 
 }
 
+# get list of parameter rank differences
+get_lparam <- function(ht, paramv = NULL){
+  # get list of parameter rank diffs
+  if(is.null(paramv)){paramv <- unique(ht[!ht$priority=="",]$parameter_name)}
+  usampv <- unique(ht$filename)
+  lparam <- lapply(paramv, get_rankdiff_sstat, ht = ht, usampv = usampv)
+  names(lparam) <- paramv
+  return(lparam)
+}
+
 #-----------------------------------------
 # settings rank differences across samples
 #-----------------------------------------
+# get unique params
 paramv <- unique(ht[!ht$priority=="",]$parameter_name)
-usampv <- unique(ht$filename)
 
-for(pi in paramv){
-  get_rankdiff_sstat(ht = ht, pi = pi, usampv = usampv)
-}
+# check for any non-zero median rank diffs among params, samples
+medv <- unlist(lapply(paramv, param_median_rankdiff, ht = ht))
+length(medv) # [1] 98
+length(medv[!medv==0]) # [1] 98
+summary(medv)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max.    NA's 
+#  0.000   0.000   0.000   2.946   0.000  17.000       9
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# get data for params with non-zero median diffs
+medv <- unlist(lapply(paramv, function(pi){
+  mediv <- param_median_rankdiff(pi = pi, ht = ht)
+  length(mediv[mediv>0])
+}))
+paramv.nzmed <- paramv[which(medv > 0)]
+paramv.nzmed
+# [1] "dye_use_membrane"                "dye_nuclei_weight"               "dye_nuclei_positive_threshold"  
+# [4] "dye_cyto_positive_threshold"     "dye_membrane_positive_threshold" "probe_contrast_threshold"       
+# [7] "probe_min_intensity"             "probe_min_spot_size"             "probe_max_spot_size"            
+# [10] "probe_copy_intensity"            "probe_segg_agg"                  "FISH Probe 3"                   
+# [13] "phenotype_name"                  "phenotype_num_criterias"         "phenotype_criteria_channel"     
+# [16] "phenotype_criteria_filter"       "IF Dye 3"  
+lparam <- get_lparam(ht, paramv = paramv.nzmed)
