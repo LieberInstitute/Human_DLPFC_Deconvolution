4d16a293dd1e5d1bc8bffa5fd8156b7028954d4d
Sean Maden <metamaden@users.noreply.github.com>
2022-09-27 08:55:29 -0400

    Update 003-4_marker-genes_upset-plots.R
    adds upset lmarker objects, save paths

--- code/004_marker-gene-expr/003-4_marker-genes_upset-plots.R
+++ code/004_marker-gene-expr/003-4_marker-genes_upset-plots.R
@@ -39,7 +39,11 @@ fm.nodrop.fpath <- file.path(save.dpath, fm.nodrop.fname)
 # upset plot save paths
 # upset plot datasets
 # withdrop
+upsetdata.withdrop.fpath <- file.path(save.dpath, 
+	"lmarkers-upsetr_gm2-fm-withdrop_dlpfc-ro1.rda")
 # no drop
+upsetdata.nodrop.fpath <- file.path(save.dpath, 
+	"lmarkers-upsetr_gm2-fm-nodrop_dlpfc-ro1.rda")
 # upset plot figures
 # with drop
 upset.withdrop.fpath <- file.path(save.dpath, "upsetr_gm2-fm-withdrop_dlpfc-ro1.png")
@@ -49,10 +53,11 @@ upset.withdrop.fpath <- file.path(save.dpath, "upsetr_gm2-fm-nodrop_dlpfc-ro1.pn
 #-----
 # load
 #-----
-# load gm2 objects -- tibbles
+# load gm2 objects -- format: tibbles
 gm2.withdrop <- get(load(gm2.withdrop.fpath))
 gm2.nodrop <- get(load(gm2.nodrop.fpath))
-# load fm objects -- lists
+
+# load fm objects -- format: lists
 fm.withdrop <- get(load(fm.withdrop.fpath))
 fm.nodrop <- get(load(fm.nodrop.fpath))
 
@@ -60,29 +65,69 @@ fm.nodrop <- get(load(fm.nodrop.fpath))
 # subset findmarkers data 
 #------------------------
 # note: select same marker count as returned from gm2
+# with drop
 ctv <- names(fm.withdrop)
 fmfilt.withdrop <- lapply(ctv, function(cti){
-	num.markers.cti <- nrow(gm2.withdrop[gm2.withdrop$cellType.target==cti,])
-	return(gm2.withdrop[[cti]][seq(num.markers.cti),])
+	nmarkersi <- nrow(gm2.withdrop[gm2.withdrop$cellType.target==cti,])
+	return(fm.withdrop[[cti]][seq(nmarkersi),])
 })
 names(fmfilt.withdrop) <- ctv
+# without drop
+ctv <- names(fm.nodrop)
+fmfilt.nodrop <- lapply(ctv, function(cti){
+	nmarkersi <- nrow(gm2.nodrop[gm2.nodrop$cellType.target==cti,])
+	return(fm.nodrop[[cti]][seq(nmarkersi),])
+})
+names(fmfilt.nodrop) <- ctv
 
 #-----------------
 # make upset lists
 #-----------------
+# get group labels
 ctv <- as.character(unique(unlist(gm2.withdrop[,2])))
 ctvv <- paste0(ctv, rep(c(".gm2", ".fm"), each = length(ctv)))
 
+# get group lists
+# with drop
+# set the findmarkers object to use here
+fm.obj <- fmfilt.withdrop 
 ldrop <- lapply(ctvv, function(cti){
-	if(gsub(".*\\.", "", cti)=="gm2"){
-		as.character(unique(
-			dati <- gm2.withdrop[gm2.withdrop[,2]==gsub("\\..*", "", cti),1]))
-			dati <- as.character(dati[,1])
-		} else{as.character(unique(
-			dati <- rownames(fm.withdrop[[gsub("\\..*", "", cti)]])))}
-		return(dati)
+	celltype <- gsub("\\..*", "", cti)
+	markermethod <- gsub(".*\\.", "", cti)
+	if(markermethod=="gm2"){
+		dati <- gm2.withdrop[gm2.withdrop[,2]==celltype,1]
+		dati <- unique(as.character(unlist(dati)))
+	} else{
+		dati <- rownames(fm.obj[[celltype]])
+		dati <- unique(as.character(dati))
+	}
+	return(dati)
 })
 names(ldrop) <- ctvv
+# without drop
+# set the findmarkers object to use here
+fm.obj <- fmfilt.nodrop
+lnodrop <- lapply(ctvv, function(cti){
+	celltype <- gsub("\\..*", "", cti)
+	markermethod <- gsub(".*\\.", "", cti)
+	if(markermethod=="gm2"){
+		dati <- gm2.nodrop[gm2.nodrop[,2]==celltype,1]
+		dati <- unique(as.character(unlist(dati)))
+	} else{
+		dati <- rownames(fm.obj[[celltype]])
+		dati <- unique(as.character(dati))
+	}
+	return(dati)
+})
+names(lnodrop) <- ctvv
+
+# check examples
+length(ldrop[[1]])==length(ldrop[[9]])
+length(lnodrop[[2]])==length(lnodrop[[10]])
+
+# save upset data lists
+save(ldrop, file = upsetdata.withdrop.fpath)
+save(lnodrop, file = upsetdata.nodrop.fpath)
 
 #----------------
 # save upset plot
