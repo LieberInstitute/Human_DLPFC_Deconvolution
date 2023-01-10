28c6a6fffc96a06731051cdab0c7ed76b5697d0a
lahuuki <lahuuki@gmail.com>
2023-01-10 17:03:46 -0500

    Update experiment tile with sce colData, and fixed SampleID typo

--- code/00_data_prep/data_standards.R
+++ code/00_data_prep/data_standards.R
@@ -1,6 +1,7 @@
 library("SummarizedExperiment")
 library("here")
 library("tidyverse")
+library("SingleCellExperiment")
 
 #### Plot Setup ####
 plot_dir = here("plots","00_data_prep","data_standards")
@@ -11,25 +12,36 @@ pos_df <- tibble(Position = c("Anterior", "Middle", "Posterior"),
                 pos = c("ant", "mid", "post"))
 
 #### Review terms in snRNA-seq ####
-sn_samples <- read.csv("/dcs04/lieber/lcolladotor/deconvolution_LIBD4030/DLPFC_snRNAseq/processed-data/03_build_sce/sample_info.csv")
-head(sn_samples)
-#        Sample file_id    region subject  round    n n_high_mito n_low_sum n_low_detect n_discard_auto
-# 1  Br2720_mid    1c-k    middle  Br2720 round1 3743         544       110          306            642
-# 2 Br2720_post    9c_k posterior  Br2720 round3 5950          26         0           13             39
-# 3  Br2743_ant    8c_k  anterior  Br2743 round3 3198         337         0            0            337
-# 4  Br2743_mid  round0    middle  Br2743 round0 3426         586        76          412            703
-# 5  Br3942_ant   11c_k  anterior  Br3942 round3 6269        1064         0            0           1064
-# 6  Br3942_mid   15c_k    middle  Br3942 round4 4309          27         0            0             27
+
+load(here("processed-data", "sce", "sce_DLPFC.Rdata"), verbose = TRUE)
+
+sn_samples <- colData(sce) |>
+  as.data.frame() |> 
+  count(Sample, SAMPLE_ID, Position, pos, round, BrNum, age, sex) 
+
+#         Sample SAMPLE_ID  Position  pos  round  BrNum   age sex    n
+# 1   Br2720_mid      1c-k    Middle  mid round1 Br2720 48.22   F 3101
+# 2  Br2720_post      9c_k Posterior post round3 Br2720 48.22   F 5911
+# 3   Br2743_ant      8c_k  Anterior  ant round3 Br2743 61.54   M 2861
+# 4   Br2743_mid    round0    Middle  mid round0 Br2743 61.54   M 2723
+# 5   Br3942_ant     11c_k  Anterior  ant round3 Br3942 47.53   M 5205
+# 6   Br3942_mid     15c_k    Middle  mid round4 Br3942 47.53   M 4282
+# 7   Br6423_ant     14c_k  Anterior  ant round4 Br6423 51.73   M 3898
+# 8  Br6423_post     12c_k Posterior post round4 Br6423 51.73   M 4067
+# 9   Br6432_ant      2c-k  Anterior  ant round1 Br6432 48.88   M 3059
+# 10  Br6471_ant      3c-k  Anterior  ant round1 Br6471 55.46   M 3212
+# 11  Br6471_mid      6c_k    Middle  mid round2 Br6471 55.46   M 4724
+# 12  Br6522_mid      4c_k    Middle  mid round2 Br6522 33.39   M 4004
+# 13 Br6522_post      5c_k Posterior post round2 Br6522 33.39   M 4275
+# 14  Br8325_ant     16c_k  Anterior  ant round5 Br8325 57.62   F 4707
+# 15  Br8325_mid     17c_k    Middle  mid round5 Br8325 57.62   F 4020
+# 16  Br8492_mid     10c_k    Middle  mid round3 Br8492 53.40   F 4997
+# 17 Br8492_post      7c_k Posterior post round2 Br8492 53.40   F 2661
+# 18  Br8667_ant     18c_k  Anterior  ant round5 Br8667 37.33   F 5774
+# 19  Br8667_mid     13c_k    Middle  mid round4 Br8667 37.33   F 4123
 
 ## need to fix:
 # round -> Round (?)
-# subject -> BrNum
-# region -> Position & capitalize first letter
-
-## temp fix 
-sn_samples <- sn_samples |>
-  rename(Position = region, BrNum = subject) |>
-  mutate(Position = str_to_title(Position))
 
 sn_n_samp <- sn_samples |>
   count(Sample, Position, BrNum) |>
@@ -37,7 +49,7 @@ sn_n_samp <- sn_samples |>
 
 #### Bulk info ####
 bulk_samples <- read.csv(here("processed-data","01_SPEAQeasy","data_info.csv"))
-head(bulk_samples)
+head(bulk_samples)[,1:7]
 #                       SAMPLE_ID       Dataset  BrNum location library_prep library_type round
 # 1 2107UNHS-0291_Br2720_Mid_Cyto 2107UNHS-0291 Br2720      Mid         Cyto        polyA     1
 # 2  2107UNHS-0291_Br2720_Mid_Nuc 2107UNHS-0291 Br2720      Mid          Nuc        polyA     1
@@ -62,7 +74,7 @@ bulk_samples |> count(library_prep, library_type)
 
 bulk_n_samp <- bulk_samples |>
   count(Sample, Position, BrNum, library_type) |>
-  mutate(data_type = paste("Bulk RNA-seq", library_type)) |>
+  mutate(data_type = paste0("Bulk RNA-seq\n", library_type)) |>
   select(-library_type)
 
 
@@ -76,6 +88,7 @@ head(halo_info)
 # 4 R1_6432P_Circle    R1   6432P Br6432   P Circle Posterior Br6432_pos     5        B 1        B1 47951
 # 5 R1_6471A_Circle    R1   6471A Br6471   A Circle  Anterior Br6471_ant     5        C 1        C1 43790
 # 6 R2_6471M_Circle    R2   6471M Br6471   M Circle    Middle Br6471_mid     4        C 1        C1 59392
+
 
 halo_samp <- halo_info |> 
   select(SAMPLE_ID, BrNum, Position, Combo) |>
@@ -111,3 +124,7 @@ experiment_tile <- all_dlpfc |>
   theme_bw()
 
 ggsave(experiment_tile, filename = here(plot_dir, "experiment_tile.png"), width = 10)
+
+ggsave(experiment_tile + 
+         theme(axis.text.x=element_text(angle=45, hjust=1)),
+       filename = here(plot_dir, "experiment_tile_small.png"), height = 5)
