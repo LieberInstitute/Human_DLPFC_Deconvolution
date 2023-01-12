ccd9000410a14e161914bb4a0064a2a45ef29fe4
Sean Maden <smaden@compute-129.cm.cluster>
2023-01-12 15:15:00 -0500

    testing deconvolution algorithms

---
+++ code/007_z-decon-tests/001-1_test-z-decon_pseudo-bulk-snrnaseq.R
@@ -0,0 +1,18 @@
+#!/usr/bin/env R
+
+#
+# Test deconvolution of Z by comparing pi estimate using the snRNAseq-derived 
+# pseudo bulk samples.
+#
+
+#----------
+# set paths
+#----------
+
+#-----
+# load
+#-----
+
+#-----------
+# set params
+#-----------
---
+++ code/007_z-decon-tests/001-2_test-z-decon_gold-standard-rnascope.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env R
+
+#
+# Test deconvolution of Z by comparing pi estimate using the gold standard from rnascope.
+#
+
+#----------
+# set paths
+#----------
+
+#-----
+# load
+#-----
+
+#-----------
+# set params
+#-----------
