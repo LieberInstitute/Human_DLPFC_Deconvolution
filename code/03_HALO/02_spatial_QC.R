16097f28636d5c774cec649932401f1977a657b0
lahuuki <lahuuki@gmail.com>
2023-01-25 16:06:50 -0500

    Use data together more and improve filenames (no star, circle prefix)

--- code/03_HALO/02_spatial_QC.R
+++ code/03_HALO/02_spatial_QC.R
@@ -13,8 +13,7 @@ if(!dir.exists(plot_dir)) dir.create(plot_dir)
 load(here("processed-data","00_data_prep","cell_colors.Rdata"), verbose = TRUE)
 
 halo_theme <- theme_bw() + 
-  theme(axis.text.x = element_text(angle = 45, hjust=1)) + 
-  labs(x = "HALO X Coord", y = "HALO Y Coord") 
+  theme(axis.text.x = element_text(angle = 45, hjust=1))
 
 blank_axis <- theme(
   axis.title.x = element_blank(),
@@ -27,28 +26,27 @@ blank_axis <- theme(
 
 #### Load Data ###
 metadata <- read.csv(here("processed-data","03_HALO","HALO_metadata.csv"))
-prop_all <- read.csv(here("processed-data","03_HALO","HALO_cell_type_proportions.csv"))
-load(here("processed-data","03_HALO","HALO_Data.Rdata"), verbose = TRUE)
+# prop_all <- read.csv(here("processed-data","03_HALO","HALO_cell_type_proportions.csv"))
+load(here("processed-data","03_HALO","halo_all.Rdata"), verbose = TRUE)
 
 # common_cols <- intersect(colnames(halo_star), colnames(halo_circle))
 # 
 # halo_all <- rbind(halo_star[,common_cols], halo_circle[,common_cols]) |> left_join(metadata)
 
 dim(halo_all)
-# [1] 1936064      34
+# [1] 1685807      40
 
 
 ## get important coordinates for each sample
 slide_position <- halo_all |> 
-  group_by(Sample_Combo) |>
+  group_by(Sample, SAMPLE_ID, Combo, subslide, i, Slide) |>
   summarize(Samp_XMax = max(XMax),
             Samp_XMin = min(XMin),
             Samp_YMax = max(YMax),
             Samp_YMin = min(YMin),
             median_X = median(XMax),
             median_Y = median(YMax)
-  )|> 
-  left_join(metadata)
+  )
 
 star_slide_guide <- slide_position |>
   filter(Combo == "Star") |>
@@ -60,11 +58,11 @@ star_slide_guide <- slide_position |>
   halo_theme+ 
   labs(x = "HALO X Coord", y = "HALO Y Coord", title = "Star Combo") 
 
-ggsave(star_slide_guide + facet_grid(i~Slide), filename = here(plot_dir, "star_slide_guide.png"))
-ggsave(star_slide_guide + facet_grid(Round~Slide), filename = here(plot_dir, "star_slide_round_guide.png"))
+ggsave(star_slide_guide + facet_grid(i~Slide), filename = here(plot_dir, "slide_guide_star.png"))
+# ggsave(star_slide_guide + facet_grid(Round~Slide), filename = here(plot_dir, "star_slide_round_guide.png"))
 
 
-circle_slide_guide <- slide_position_circle |> 
+circle_slide_guide <- slide_position |> 
   filter(Combo == "Circle") |>
   ggplot(aes(x = median_X, y = median_Y)) +
   geom_rect(aes(xmin=Samp_XMin, xmax=Samp_XMax,
@@ -74,43 +72,27 @@ circle_slide_guide <- slide_position_circle |>
   halo_theme+ 
   labs(x = "HALO X Coord", y = "HALO Y Coord", title = "Circle Combo") 
 
-ggsave(circle_slide_guide + facet_grid(i~Round), filename = here(plot_dir, "circle_slide_guide.png"))
-ggsave(circle_slide_guide + facet_grid(Round~Slide), filename = here(plot_dir, "circle_slide_round_guide.png"))
+ggsave(circle_slide_guide + facet_grid(i~Slide), filename = here(plot_dir, "slide_guide_circle.png"))
+# ggsave(circle_slide_guide + facet_grid(Round~Slide), filename = here(plot_dir, "circle_slide_round_guide.png"))
 
 
 ## Plot Slides
-star_slides <- halo_star |> 
+all_slides <- halo_all |> 
   ggplot() +
   geom_rect(aes(xmin=XMin, xmax=XMax,
                 ymin = YMin, ymax = YMax, 
                 fill = cell_type)) +
-  facet_grid(i~Slide) +
-  geom_text(data = slide_position_star, aes(x = median_X, y = Samp_YMax + 100, label = Sample), size = 2)+
+  facet_grid(i~Slide+Combo) +
+  geom_text(data = slide_position, aes(x = median_X, y = Samp_YMax + 100, label = SAMPLE_ID), size = 2)+
   scale_fill_manual(values = cell_type_colors_halo) +
   halo_theme+ 
   labs(x = "HALO X Coord", y = "HALO Y Coord", title = "Star Combo") 
 
-ggsave(star_slides, filename = here(plot_dir, "star_slides.png"))
+ggsave(all_slides, filename = here(plot_dir, "slides_all.png"), width = 12)
 
 
-circle_slides <- halo_circle |> 
-  ggplot() +
-  geom_rect(aes(xmin=XMin, xmax=XMax,
-                ymin = YMin, ymax = YMax, 
-                fill = cell_type)) +
-  geom_text(data = slide_position_circle, aes(x = median_X, y = Samp_YMax + 100, label = Sample), size = 2)+
-  facet_grid(i~Slide) +
-  scale_fill_manual(values = cell_type_colors_halo) +
-  halo_theme+ 
-  labs(x = "HALO X Coord", y = "HALO Y Coord", title = "Circle Combo") 
-
-ggsave(circle_slides, filename = here(plot_dir, "circle_slides.png"))
-
-ggsave(star_slides + theme(legend.position = "None") + circle_slides, filename = here(plot_dir, "ALL_slides.png"), width = 12)
-
-
-
-star_samples <- halo_star |> 
+star_samples <- halo_all |> 
+  filter(Combo == "Star") |>
   ggplot() +
   geom_rect(aes(xmin=XMin, xmax=XMax,
                 ymin = YMin, ymax = YMax, 
@@ -118,10 +100,13 @@ star_samples <- halo_star |>
   facet_wrap(~Sample, scales = 'free_y') +
   theme_bw() +
   scale_fill_manual(values = cell_type_colors_halo) 
-ggsave(star_samples, filename = here(plot_dir, "star_samples.png"), height = 10, width = 18)
+
+ggsave(star_samples, filename = here(plot_dir, "nuc_samples_Star.png"), height = 10, width = 18)
+ggsave(star_samples, filename = here(plot_dir, "nuc_samples_Star.pdf"), height = 10, width = 18)
 
 
-circle_samples <- halo_circle |> 
+circle_samples <- halo_all |> 
+  filter(Combo == "Circle") |>
   ggplot() +
   geom_rect(aes(xmin=XMin, xmax=XMax,
                 ymin = YMin, ymax = YMax, 
@@ -130,11 +115,12 @@ circle_samples <- halo_circle |>
   theme_bw() +
   scale_fill_manual(values = cell_type_colors_halo)
 
-ggsave(circle_samples, filename = here(plot_dir, "circle_samples.png"), height = 10, width = 16)
+ggsave(circle_samples, filename = here(plot_dir, "nuc_samples_Circle.png"), height = 10, width = 16)
+ggsave(circle_samples, filename = here(plot_dir, "nuc_samples_Circle.pdf"), height = 10, width = 16)
 
 ## plot ct only
-star_samples_ct <- halo_star |> 
-  filter(cell_type != 'Other') |>
+star_samples_ct <- halo_all |> 
+  filter(Combo == "Star", cell_type != 'Other') |>
   ggplot() +
   geom_rect(aes(xmin=XMin, xmax=XMax,
                 ymin = YMin, ymax = YMax, 
@@ -143,11 +129,12 @@ star_samples_ct <- halo_star |>
   theme_bw() +
   scale_fill_manual(values = cell_type_colors_halo) 
 
-ggsave(star_samples_ct, filename = here(plot_dir, "star_samples_ct.png"), height = 10, width = 18)
+ggsave(star_samples_ct, filename = here(plot_dir, "nuc_ct_samples_star.png"), height = 10, width = 18)
+ggsave(star_samples_ct, filename = here(plot_dir, "nuc_ct_samples_star.pdf"), height = 10, width = 18)
 
 
-circle_samples_ct <- halo_circle |> 
-  filter(cell_type != 'Other')|> 
+circle_samples_ct <- halo_all |> 
+  filter(Combo == "Circle", cell_type != 'Other') |>
   ggplot() +
   geom_rect(aes(xmin=XMin, xmax=XMax,
                 ymin = YMin, ymax = YMax, 
@@ -156,7 +143,8 @@ circle_samples_ct <- halo_circle |>
   theme_bw() +
   scale_fill_manual(values = cell_type_colors_halo)
 
-ggsave(circle_samples_ct, filename = here(plot_dir, "circle_samples_ct.png"), height = 10, width = 16)
+ggsave(circle_samples_ct, filename = here(plot_dir, "nuc_ct_samples_circle.png"), height = 10, width = 16)
+ggsave(circle_samples_ct, filename = here(plot_dir, "nuc_ct_samples_circle.pdf"), height = 10, width = 16)
 
 
 ## ALL Samples - need to normalize data
@@ -174,7 +162,7 @@ all_samples_ct <- halo_all |>
   theme(strip.background = element_blank(),
         strip.text = element_blank())
 
-ggsave(all_samples_ct, filename = here(plot_dir, "ALL_samples_ct.png"), height = 12, width = 16)
+ggsave(all_samples_ct, filename = here(plot_dir, "nuc_samples_all.pdf"), height = 12, width = 16)
 
 
 
@@ -219,7 +207,7 @@ circle_nuc_area_hex <- ggplot(halo_circle) +
 ggsave(circle_nuc_area_hex, filename = here(plot_dir, "circle_hex_nuc_area.png"), height = 10, width = 18)
 
 #### print each with grid ####
-halo_all$Sample_Combo <- factor(halo_all$Sample_Combo)
+halo_all$SAMPLE_ID <- factor(halo_all$SAMPLE_ID)
 
 walk(levels(halo_all$Sample_Combo), function(s) {
   message(s)
