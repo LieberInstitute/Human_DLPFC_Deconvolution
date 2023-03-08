ea138aaed63ad3fa21940a632ce682e67fcc49d3
lahuuki <lahuuki@gmail.com>
2023-03-08 13:49:13 -0500

    Update pipe, revisit bulk qc code

--- code/02_quality_control/quality_control_check.R
+++ code/02_quality_control/quality_control_check.R
@@ -12,8 +12,8 @@ load("/dcl01/ajaffe/data/lab/brain_swap/count_data/RNAseq_Collection_postQC_n576
 dim(rse_gene)
 # [1] 58037  5761
 
-bsp2_library_type <- read_excel('/dcl01/lieber/ajaffe/lab/brainseq_phase2/misc/LIBD_PhaseII_HIPPO_RiboZero_sample_list_01_28_2015.xlsx') %>%
-  mutate(RNum = paste0("R", RNum)) %>%
+bsp2_library_type <- read_excel('/dcl01/lieber/ajaffe/lab/brainseq_phase2/misc/LIBD_PhaseII_HIPPO_RiboZero_sample_list_01_28_2015.xlsx') |>
+  mutate(RNum = paste0("R", RNum)) |>
   select(RNum, library_type = Protocol)
 
 table(bsp2_library_type$library_type)
@@ -21,9 +21,9 @@ table(bsp2_library_type$library_type)
 # 47          405 
 
 
-pd_big <- as.data.frame(colData(rse_gene)) %>%
-  left_join(bsp2_library_type) %>%
-  replace_na(list(library_type = "RiboZeroGold")) %>%
+pd_big <- as.data.frame(colData(rse_gene)) |>
+  left_join(bsp2_library_type) |>
+  replace_na(list(library_type = "RiboZeroGold")) |>
   mutate(library_type = ifelse(Dataset == "BrainSeq_Phase1", "polyA", library_type))
 
 table(pd_big$library_type)
@@ -50,17 +50,25 @@ colnames(pd_big)
 load(here("processed-data","01_SPEAQeasy","round2_v40_2022-07-06","rse","rse_gene.Rdata"), verbose = TRUE)
 pd_new <- as.data.frame(colData(rse_gene))
 
-## pass fail matrix
-pass_fail <- pd_new %>%
-  select(SAMPLE_ID,Dataset, basic_statistics:adapter_content) %>%
-  pivot_longer(!c(SAMPLE_ID,Dataset), names_to = "Test")
+pd_new |>
+  count(Dataset, library_type)
+#         Dataset library_type  n
+# 1 2107UNHS-0291        polyA 12
+# 2 2107UNHS-0293 RiboZeroGold 12
+# 3    AN00000904        polyA 44
+# 4    AN00000906 RiboZeroGold 45
 
-pass_fail %>% group_by(Test) %>%
+## pass fail matrix
+pass_fail <- pd_new |>
+  select(SAMPLE_ID,library_type, Dataset, basic_statistics:adapter_content) |>
+  pivot_longer(!c(SAMPLE_ID,library_type,Dataset), names_to = "Test")
+
+pass_fail |> group_by(Test) |>
   count(value)
 
 
-pass_fail %>% group_by(Dataset, Test) %>%
-  count(value) %>%
+pass_fail |> group_by(Dataset, Test) |>
+  count(value) |>
   filter(value=="FAIL")
 
 # Dataset       Test                        value     n
@@ -80,22 +88,22 @@ pass_fail %>% group_by(Dataset, Test) %>%
 pass_fail_plot <- ggplot(pass_fail, aes(x = Test, y = SAMPLE_ID)) +
   geom_tile(aes(fill = value)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  facet_wrap(~Dataset, scales = "free_y")
+  facet_grid(library_type~Dataset, scales = "free_y")
 
 ggsave(pass_fail_plot, filename = here("plots","02_quality_control","pass_fail_tile.png"), height = 10, width = 10)
 
 ## Subset and QC data
 qc_variables <- c("numReads", "numMapped", "numUnmapped", "overallMapRate", "concordMapRate", "totalMapped", "mitoMapped","mitoRate", "rRNA_rate", "totalAssignedGene")
 
-pd_big_qc_long <- pd_big %>%
-  select(SAMPLE_ID, Dataset, library_type, all_of(qc_variables)) %>%
-  pivot_longer(!c(SAMPLE_ID, Dataset, library_type), names_to = "qc_var") %>%
+pd_big_qc_long <- pd_big |>
+  select(SAMPLE_ID, Dataset, library_type, all_of(qc_variables)) |>
+  pivot_longer(!c(SAMPLE_ID, Dataset, library_type), names_to = "qc_var") |>
   mutate(`Data Status` = "old")
 
 
-pd_new_qc_long <- pd_new %>%
-  select(SAMPLE_ID, Dataset, library_type, all_of(qc_variables)) %>%
-  pivot_longer(!c(SAMPLE_ID, Dataset, library_type), names_to = "qc_var") %>%
+pd_new_qc_long <- pd_new |>
+  select(SAMPLE_ID, Dataset, library_type, all_of(qc_variables)) |>
+  pivot_longer(!c(SAMPLE_ID, Dataset, library_type), names_to = "qc_var") |>
   mutate(`Data Status` = "new")
 
 pd_qc_long <- rbind(pd_new_qc_long, pd_big_qc_long)
@@ -110,11 +118,11 @@ ggsave(qc_boxplots, filename = here("plots", "02_quality_control", "qc_boxplots.
 
 
 #### Just New data plotly boxplots###
-pd_new_qc_key <- pd_new_qc_long %>%
-  mutate(anno = ss(SAMPLE_ID, "-", 3)) %>%
+pd_new_qc_key <- pd_new_qc_long |>
+  mutate(anno = ss(SAMPLE_ID, "-", 3)) |>
   highlight_key(~anno)
 
-qc_new_boxplots <-  pd_new_qc_key %>%
+qc_new_boxplots <-  pd_new_qc_key |>
   ggplot(aes(x = Dataset, y = value)) +
   geom_boxplot(aes(fill = `library_type`), outlier.shape = NA) +
   geom_jitter() +
@@ -137,4 +145,4 @@ htmlwidgets::saveWidget(highlight(qc_new_boxplots_plotly,
                         file = here("plots", "02_quality_control", "qc_new_boxplots.html"))
 
 
-pd_new %>% filter(totalMapped < 5e7)
+pd_new |> filter(totalMapped < 5e7)
