e1214fca93c73c006747c8727a098226a61ec36e
lahuuki <lahuuki@gmail.com>
2023-04-17 15:17:51 -0400

    Style 05_LIBD_deconvo/*.R

--- code/05_LIBD_deconvo/01_previous_LIBD_deconvo.R
+++ code/05_LIBD_deconvo/01_previous_LIBD_deconvo.R
@@ -1,4 +1,3 @@
-
 library("tidyverse")
 library("sessioninfo")
 library("here")
@@ -7,15 +6,16 @@ library("here")
 
 #### Compile data ####
 
-deconvo_data_paths <- list(BipSeq = "/dcl01/lieber/ajaffe/lab/zandiHyde_bipolar_rnaseq/deconvolution/est_prop_Bisque.Rdata",
-                     BSP3 = "/dcl01/lieber/RNAseq/Datasets/BrainSeq_hg38/Phase3/caudate_BS3P/est_prop.Rdata",
-                     MDDseq = "/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/deconvolution/data/est_prop_Bisque.Rdata",
-                     # AANRI = "/dcl01/beer/lieber/kynon_temp/control_ancestry/analysis/deconvolution",
-                     GTEx = "/dcl01/lieber/ajaffe/lab/deconvolution_bsp2/data/GTEx_est_prop.Rdata"
-                     # Suicide = #local
-                     #  BSP1 = 
-                     #  BSP2 = 
-                     #  Degradation =
+deconvo_data_paths <- list(
+    BipSeq = "/dcl01/lieber/ajaffe/lab/zandiHyde_bipolar_rnaseq/deconvolution/est_prop_Bisque.Rdata",
+    BSP3 = "/dcl01/lieber/RNAseq/Datasets/BrainSeq_hg38/Phase3/caudate_BS3P/est_prop.Rdata",
+    MDDseq = "/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/deconvolution/data/est_prop_Bisque.Rdata",
+    # AANRI = "/dcl01/beer/lieber/kynon_temp/control_ancestry/analysis/deconvolution",
+    GTEx = "/dcl01/lieber/ajaffe/lab/deconvolution_bsp2/data/GTEx_est_prop.Rdata"
+    # Suicide = #local
+    #  BSP1 =
+    #  BSP2 =
+    #  Degradation =
 )
 
 
