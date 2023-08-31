25a453d101f354dcf8361cfb3a48c675a0e46051
Louise Huuki <lahuuki@gmail.com>
2023-08-31 15:27:58 -0400

    Setup basic MuSiC script

--- code/08_bulk_deconvolution/02_deconvolution_MuSiC.R
+++ code/08_bulk_deconvolution/02_deconvolution_MuSiC.R
@@ -1,13 +1,13 @@
 
 library("SingleCellExperiment")
-library("xbioc")
-library("BisqueRNA")
+library("MuSiC")
 library("here")
 library("sessioninfo")
 
 #### load data ####
 ## load bulk data
-here("processed-data","rse", "rse_gene.Rdata")
+load(here("processed-data","rse", "rse_gene.Rdata"), verbose = TRUE)
+rownames(rse_gene) <- rowData(rse_gene)$ensemblID
 
 ## sce data
 load(here("processed-data", "sce", "sce_DLPFC.Rdata"), verbose = TRUE)
@@ -15,8 +15,10 @@ load(here("processed-data", "sce", "sce_DLPFC.Rdata"), verbose = TRUE)
 sce <- sce[,sce$cellType_broad_hc != "Ambiguous"]
 sce$cellType_broad_hc <- droplevels(sce$cellType_broad_hc)
 
+rownames(sce) <- rowData(sce)$gene_id
+
 ## find common genes
-common_genes <- intersect(rowData(sce)$gene_id, rowData(rse_gene_brain_gtex)$ensembl)
+common_genes <- intersect(rowData(sce)$gene_id, rowData(rse_gene)$ensemblID)
 length(common_genes)
 
 ## load marker gene data
@@ -24,20 +26,29 @@ load(here("processed-data", "06_marker_genes", "03_find_markers_broad", "marker_
 # marker_stats
 
 marker_stats |>
-  filter(gene %in% common_genes,
+  dplyr::filter(gene %in% common_genes,
          rank_ratio <= 25) |>
-  count(cellType.target)
+  dplyr::count(cellType.target)
 
-# cellType.target     n
-# <fct>           <int>
-# 1 Astro              25
-# 2 EndoMural          25
-# 3 Micro              25
-# 4 Oligo              23
-# 5 OPC                23
-# 6 Excit              24
-# 7 Inhib              25
 
 markers <- marker_stats |> 
-  filter(gene %in% common_genes, rank_ratio <= 25) |>
-  pull(gene)
+  dplyr::filter(gene %in% common_genes, rank_ratio <= 25) |>
+  dplyr::pull(gene)
+
+#### Run MuSiC ####
+message(Sys.time(), " - MuSiC deconvolution")
+est_prop_music <- music_prop(bulk.mtx = assays(rse_gene)$counts,
+                             sc.sce = sce,
+                             markers = markers,
+                             clusters = "cellType_broad_hc",
+                             samples = "Sample")
+
+save(est_prop_music, file = here("processed-data","08_bulk_deconvolution","est_prop_music.Rdata"))
+
+# sgejobs::job_single('02_deconvolution_MuSiC', create_shell = TRUE, memory = '25G', command = "Rscript 02_deconvolution_MuSiC.R")
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
