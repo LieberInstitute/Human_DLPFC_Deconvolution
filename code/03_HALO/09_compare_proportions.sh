75d6364b525e37daf947dbfe0d9cb5da2a78eb35
lahuuki <lahuuki@gmail.com>
2023-09-27 14:50:14 -0400

    Update sh files and logs

---
+++ code/03_HALO/09_compare_proportions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+#SBATCH --output=logs/09_compare_proportions.txt
+#SBATCH --error=logs/09_compare_proportions.txt
+#SBATCH --partition=shared
+#SBATCH --job-name=compare_proportions
+#SBATCH --mem=5GB
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R/4.3
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript 09_compare_proportions.R
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
