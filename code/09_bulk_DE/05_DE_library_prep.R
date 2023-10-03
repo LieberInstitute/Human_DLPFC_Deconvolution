d92a595327dcedbc227b3c4158d847ce8652fcf1
lahuuki <lahuuki@gmail.com>
2023-10-03 12:03:24 -0400

    Edit to run DE library prep, by library type - export F stats but save eBayes, create sh file

--- code/09_bulk_DE/05_DE_library_prep.R
+++ code/09_bulk_DE/05_DE_library_prep.R
@@ -10,7 +10,7 @@ library("sessioninfo")
 #### Set up ####
 
 ## dirs
-data_dir <- here("processed-data", "09_bulk_DE", "04_DE_library-type")
+data_dir <- here("processed-data", "09_bulk_DE", "05_DE_library-prep")
 if(!dir.exists(data_dir)) dir.create(data_dir, recursive = TRUE)
 
 #### load data ####
@@ -21,12 +21,14 @@ names(rse_paths) <- features
 
 all(file.exists(rse_paths))
 
+## for testing
+# rse_paths <- rse_paths["gene"]
+
 rse_list <- lapply(rse_paths, function(x) get(load(x)))
 
 ## edit rse list: factor sample, split library_prep
-
 rse_list <- map(rse_list, function(rse){
-  rse <- map(splitit(rse$library_prep), ~rse[,.x])
+  rse <- map(splitit(rse$library_type), ~rse[,.x])
   return(rse)
 })
 
@@ -34,19 +36,16 @@ rse_list <- map(rse_list, function(rse){
 mod <- map(rse_list$gene, function(rse){
   pd <- as.data.frame(colData(rse))
   pd$Sample <- factor(pd$Sample)
-  mod <- model.matrix(~Sample + library_type, data = pd)
+  mod <- model.matrix(~Sample + library_prep, data = pd)
   return(mod)
 })
 
 map(mod, dim)
-# $Bulk
-# [1] 38 20
+# $polyA
+# [1] 55 21
 # 
-# $Cyto
-# [1] 37 20
-# 
-# $Nuc
-# [1] 35 20
+# $RiboZeroGold
+# [1] 55 21
 
 colnames(mod[[1]])
 
@@ -59,16 +58,16 @@ DE_out <- map2(rse_list, names(rse_list), function(rse, feat_name){
     message(Sys.time(), ' - Running DE ', feat_name, " + ", prep_name)
     stopifnot(ncol(rse_prep) == nrow(mod))
     
-    outDE <- run_DE(rse = rse_prep, model = mod, coef = "library_typeRiboZeroGold", run_voom = feat_name != "tx")
+    outDE <- run_DE(rse = rse_prep, model = mod, coef = c("library_prepCyto", "library_prepNuc"), run_voom = feat_name != "tx", save_eBayes = TRUE)
     
     message(Sys.time(), " - Saving")
-    write.csv(outDE, file = here(data_dir, paste0("DE_library-type_", feat_name, "_",prep_name,".csv")), row.names = FALSE)
+    write.csv(outDE$topTable, file = here(data_dir, paste0("DE_library-type_", feat_name, "_",prep_name,".csv")), row.names = FALSE)
     
     return(outDE)
   })
 })
 
-head(DE_out$gene$Bulk)
+head(DE_out$gene$RiboZeroGold$topTable)
 
 save(DE_out, file = here(data_dir, "DE_library-type.Rdata"))
 
