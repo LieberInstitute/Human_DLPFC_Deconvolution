ac042baa7ddec092dbb9ab220b2255151976bbad
lahuuki <lahuuki@gmail.com>
2023-10-11 14:45:20 -0400

    Fix map, add massages

--- code/09_bulk_DE/08_DREAM_library-type.R
+++ code/09_bulk_DE/08_DREAM_library-type.R
@@ -52,39 +52,41 @@ form <- ~library_type + (1|BrNum) + mitoRate + rRNA_rate + totalAssignedGene
 #### RUN DE ####
 source(here("code", "09_bulk_DE","run_DE.R"))
 
-DREAM_library_type <- pmap(list(rse_prep = rse, prep_name = names(rse)), function(rse_prep, prep_name, mod){
+DREAM_library_type <- map2(rse_list, names(rse_list), function(rse, prep_name){
   
   message(Sys.time(), ' - Running DE ', prep_name)
   
   dge = DGEList(assays(rse)$counts)
-  dge = calcNormFactors( dge )
+  dge = calcNormFactors(dge)
   
   metadata = as.data.frame(colData(rse))
   
   param = SnowParam(4, "SOCK", progressbar=TRUE)
   # estimate weights using linear mixed model of dream
-  vobjDream = voomWithDreamWeights(dge, form, metadata, BPPARAM=param )
+  message(Sys.time(), ' - voomWithDreamWeights')
+  vobjDream = voomWithDreamWeights(dge, form, metadata, BPPARAM=param)
   
   # Fit the dream model on each gene
   # By default, uses the Satterthwaite approximation for the hypothesis test
-  fitmm = dream( vobjDream, form, metadata )
+  message(Sys.time(), ' - Dream')
+  fitmm = dream(vobjDream, form, metadata)
+  message(Sys.time(), ' - eBayes')
   fitmm = eBayes(fitmm)
   
   # Examine design matrix
   head(fitmm$design, 3)
   
-  outDE <- topTable( fitmm, coef='Disease1', number=Inf , sort.by = "none")
+  outDE <- topTable( fitmm, coef="library_typeRiboZeroGold", number=Inf , sort.by = "none")
   
   message(Sys.time(), " - Saving")
-  try(write.csv(outDE, file = here(data_dir, paste0("DE_library-type_", feat_name, "_",prep_name,".csv")), row.names = FALSE))
+  try(write.csv(outDE, file = here(data_dir, paste0("DREAM_library-type_gene_",prep_name,".csv")), row.names = FALSE))
   
   return(outDE)
 })
 
-
 save(DREAM_library_type, file = here(data_dir, "DREAM_library-type.Rdata"))
 
-# slurmjobs::job_single(name = "01_pseudobulk_sn", memory = "10G", cores = 1, create_shell = TRUE, command = "Rscript 01_pseudobulk_sn.R")
+# slurmjobs::job_single(name = "08_DREAM_library-type", memory = "10G", cores = 1, create_shell = TRUE, command = "Rscript 08_DREAM_library-type.R")
 
 ## Reproducibility information
 print("Reproducibility information:")
