578ba340283ce49b11a2cacb65739919a48773af
lahuuki <lahuuki@gmail.com>
2023-10-18 10:27:13 -0400

    Fix coef bug, add log

---
+++ code/10_bulk_vs_sn_DE/03_DREAM_sn_v_bulk.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH -p shared
+#SBATCH --mem-per-cpu=10G
+#SBATCH --job-name=03_DREAM_sn_v_bulk
+#SBATCH -c 1
+#SBATCH -o logs/03_DREAM_sn_v_bulk.txt
+#SBATCH -e logs/03_DREAM_sn_v_bulk.txt
+#SBATCH --mail-type=ALL
+
+set -e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## Load the R module
+module load conda_R
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript 03_DREAM_sn_v_bulk.R
+
+echo "**** Job ends ****"
+date
+
+## This script was made using slurmjobs version 0.99.0
+## available from http://research.libd.org/slurmjobs/
