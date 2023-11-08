f5ad557274f49f6b4d06b56e1828dca349643d00
lahuuki <lahuuki@gmail.com>
2023-11-08 18:07:05 -0500

    Resize sn tSNA

--- code/10_bulk_vs_sn_DE/06_sn_reduced_dims.R
+++ code/10_bulk_vs_sn_DE/06_sn_reduced_dims.R
@@ -11,8 +11,7 @@ load(here("processed-data", "00_data_prep", "cell_colors.Rdata"), verbose = TRUE
 
 my_theme <- theme_bw() +
   theme(
-    text = element_text(size = 15),
-    panel.grid.major = element_blank(), panel.grid.minor = element_blank()
+    text = element_text(size = 15)
   )
 
 ## load our DLPFC data
@@ -25,11 +24,12 @@ table(sce$cellType_broad_hc)
 
 TSNE_cellTypes_broad <- ggcells(sce, mapping = aes(x = TSNE.1, y = TSNE.2, colour = cellType_broad_hc)) +
   geom_point(size = 0.2, alpha = 0.3) +
-  scale_color_manual(values = cell_type_colors_broad, name = "Cell Type") +
+  scale_color_manual(values = cell_type_colors_broad, name = "Cell\nType") +
   coord_equal() +
-  labs(x = "TSNE Dimension 1", y = "TSNE Dimension 2") +
+  labs(x = "tSNE Dimension 1", y = "tSNE Dimension 2") +
   my_theme +
-  guides(color = guide_legend(override.aes = list(size = 2, alpha = 1)))
+  guides(color = guide_legend(override.aes = list(size = 2, alpha = 1), nrow = 3)) +
+  theme(legend.position = "bottom", legend.text=element_text(size=10))
 
-ggsave(TSNE_cellTypes_broad, filename = here(plot_dir, "TSNE_cellTypes_broad.png"), width = 5, height = 3.75)
+ggsave(TSNE_cellTypes_broad, filename = here(plot_dir, "TSNE_cellTypes_broad.png"), width = 4, height = 5)
 
