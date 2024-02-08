bb7c9ac2c2e3a31529d4a01af5b1c64a2288c345
Louise Huuki <lahuuki@gmail.com>
2024-02-08 15:53:51 -0500

    Add new MuSic sh files

---
+++ code/08_bulk_deconvolution/02_deconvolution_MuSiC_MeanRatio_top25.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH -p shared
+#SBATCH --mem=25G
+#SBATCH --job-name=02_deconvolution_MuSiC_MeanRatio_top25
+#SBATCH -c 1
+#SBATCH -o logs/02_deconvolution_MuSiC_MeanRatio_top25.txt
+#SBATCH -e logs/02_deconvolution_MuSiC_MeanRatio_top25.txt
+#SBATCH --mail-type=ALL
+
+set -e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## Load the R module
+module load conda_R/4.3
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript 02_deconvolution_MuSiC.R MeanRatio_top25 ../../processed-data/08_bulk_deconvolution/markers_MeanRatio_top25.txt
+
+echo "**** Job ends ****"
+date
+
+## This script was made using slurmjobs version 1.1.0
+## available from http://research.libd.org/slurmjobs/
