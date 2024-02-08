5ee7f827b86d78ec4c3b8ae8832b7c435a1572e4
Louise Huuki <lahuuki@gmail.com>
2024-02-08 15:46:04 -0500

    use args for hspe

--- code/08_bulk_deconvolution/05_deconvolution_hspe.R
+++ code/08_bulk_deconvolution/05_deconvolution_hspe.R
@@ -7,12 +7,17 @@ library("jaffelab")
 library("here")
 library("sessioninfo")
 
+## get args
+args = commandArgs(trailingOnly=TRUE)
+marker_label <- args[1]
+marker_file <- NULL
 
-#### Run on our data ####
+## not using txt list of marker genes, methods needs named list
+stopifnot(marker_label %in% c("MeanRatio_top25", "1vALL_top25", "FULL"))
 
-# output_dir <- here("processed-data","08_bulk_deconvolution", "05_deconvoltion_hspe")
-# if(!file.exists(output_dir)) dir.create(output_dir)
-
+#### data output folder ####
+data_dir <- here("processed-data","08_bulk_deconvolution", "05_deconvolution_hspe")
+if(!dir.exists(data_dir)) dir.create(data_dir)
 
 #### load data ####
 ## load bulk data
@@ -21,10 +26,6 @@ dim(rse_gene)
 # [1] 21745   110
 
 rownames(rse_gene) <- rowData(rse_gene)$ensemblID
-
-## sce data
-# load(here("processed-data", "sce", "sce_DLPFC.Rdata"), verbose = TRUE)
-# sce <- sce[,sce$cellType_broad_hc != "Ambigious"]
 
 ## pseudobulk sce data
 sce_pb <- readRDS(here("processed-data", "sce","sce_broad_pseudobulk.rds"))
@@ -35,8 +36,10 @@ table(sce_pb$cellType_broad_hc)
 ## use ensemblIDs
 rownames(sce_pb) <- rowData(sce_pb)$gene_id
 
-common_genes <- intersect(rownames(rse_gene), rownames(sce_pb))
+## find common genes
+common_genes <- intersect(rowData(sce_pb)$gene_id, rowData(rse_gene)$ensemblID)
 length(common_genes)
+# [1] 13311
 
 # we can instead explicitly pass a list of markers to hspe specifying the marker genes
 # elements of the list correspond one to each cell type in the same order specified either in elements of pure_samples
@@ -46,47 +49,56 @@ pure_samples = rafalib::splitit(sce_pb$cellType_broad_hc)
 mixture_samples = t(assays(rse_gene)$logcounts[common_genes,])
 reference_samples = t(assays(sce_pb)$logcounts[common_genes,])
 
-ncol(mixture_samples) == ncol(reference_samples)
+stopifnot(ncol(mixture_samples) == ncol(reference_samples))
 
-message(Sys.time(), "- hspe")
-est_prop_hspe = hspe(Y = mixture_samples, 
-                     reference = reference_samples,
-                     pure_samples = pure_samples,
-                     seed =10524)
+est_prop_hspe <- NULL
+
+if(marker_label == "FULL"){
+  
+  message(Sys.time(), "- hspe")
+  ## n_markers...If not specified then top 10% of genes are used. 1vALL ratio method
+  est_prop_hspe = hspe(Y = mixture_samples, 
+                       reference = reference_samples,
+                       pure_samples = pure_samples,
+                       seed =10524)
+
+} elseif(marker_label %in% c("MeanRatio_top25", "1vALL_top25")){ ## Run with our markers 
+  
+  ## load marker gene data
+  load(here("processed-data", "06_marker_genes", "03_find_markers_broad", "marker_stats_broad.Rdata"), verbose = TRUE)
+  marker_tab <- NULL
+  
+  if(marker_label == "MeanRatio_top25"){
+    marker_tab <- marker_stats |> 
+      dplyr::filter(gene %in% common_genes, rank_ratio <= 25) 
+  } else {
+    marker_tab <- marker_stats |> 
+      dplyr::filter(gene %in% common_genes, rank_marker <= 25) 
+  }
+  
+  marker_genes <- purrr::map(rafalib::splitit(marker_tab$cellType.target), ~marker_tab$gene[.x])
+  
+  marker_genes <- marker_genes[names(pure_samples)]
+  
+  message(Sys.time(), "- hspe w/ markers ", marker_label)
+  
+  ## notes
+  
+  ## markers: Marker gene indices...list is a vector of indicies (columns of ‘Y’) that will be considered markers of that particular type.
+  
+  est_prop_hspe = hspe(Y = mixture_samples, 
+                       reference = reference_samples,
+                       pure_samples = pure_samples,
+                       markers = marker_genes,
+                       seed = 10524)
+} else stop()
 
 message(Sys.time(), "- Saving")
-save(est_prop_hspe, file = here("processed-data","08_bulk_deconvolution","est_prop_hspe.Rdata"))
+save(est_prop_hspe, file = here(data_dir,paste0("est_prop_hspe-",marker_label,".Rdata")))
 
-#### Run with our markers ####
-
-## load marker gene data
-load(here("processed-data", "06_marker_genes", "03_find_markers_broad", "marker_stats_broad.Rdata"), verbose = TRUE)
-# marker_stats
-
-marker_stats |>
-  dplyr::filter(gene %in% common_genes,
-                rank_ratio <= 25) |>
-  dplyr::count(cellType.target)
-
-marker_tab <- marker_stats |> 
-  dplyr::filter(gene %in% common_genes, rank_ratio <= 25) 
-
-marker_genes <- purrr::map(rafalib::splitit(marker_tab$cellType.target), ~marker_tab$gene[.x])
-
-marker_genes <- marker_genes[names(pure_samples)]
-
-message(Sys.time(), "- hspe w/ markers")
-est_prop_hspe = hspe(Y = mixture_samples, 
-                     reference = reference_samples,
-                     pure_samples = pure_samples,
-                     markers = marker_genes,
-                     seed = 10524)
-
-message(Sys.time(), "- Saving")
-save(est_prop_hspe, file = here("processed-data","08_bulk_deconvolution","est_prop_hspe_markers.Rdata"))
-
-
-# slurmjobs::job_single(name = "05_deconvolution_hspe", memory = "25G", cores = 1, create_shell = TRUE, command = "Rscript 05_deconvolution_hspe.R")
+# slurmjobs::job_single('05_deconvolution_hspe_FULL', create_shell = TRUE, memory = '25G', command = "Rscript 05_deconvolution_hspe.R FULL")
+# slurmjobs::job_single('05_deconvolution_hspe_MeanRatio_top25', create_shell = TRUE, memory = '25G', command = "Rscript 05_deconvolution_hspe.R MeanRatio_top25")
+# slurmjobs::job_single('05_deconvolution_hspe_1vALL_top25', create_shell = TRUE, memory = '25G', command = "Rscript 05_deconvolution_hspe.R 1vALL_top25")
 
 ## Reproducibility information
 print("Reproducibility information:")
