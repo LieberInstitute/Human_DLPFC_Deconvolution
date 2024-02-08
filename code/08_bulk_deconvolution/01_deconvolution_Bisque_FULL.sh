7167f57b619e8d758b80e407e74f5a3b8a941078
Louise Huuki <lahuuki@gmail.com>
2024-02-08 16:10:06 -0500

    Use "module load conda_R/4.3.x" in sh files

--- code/08_bulk_deconvolution/01_deconvolution_Bisque_FULL.sh
+++ code/08_bulk_deconvolution/01_deconvolution_Bisque_FULL.sh
@@ -17,7 +17,7 @@ echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SGE_TASK_ID}"
 
 ## Load the R module (absent since the JHPCE upgrade to CentOS v7)
-module load conda_R
+module load conda_R/4.3.x
 
 ## List current modules for reproducibility
 module list
