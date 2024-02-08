d73373ef798f7b5e4873b99538927365d55f93a4
Louise Huuki <lahuuki@gmail.com>
2024-02-08 12:06:00 -0500

    pull top25 1vALL markers to txt for input in deconvo

--- code/08_bulk_deconvolution/00_pull_markers.R
+++ code/08_bulk_deconvolution/00_pull_markers.R
@@ -30,12 +30,11 @@ load(here("processed-data", "06_marker_genes", "03_find_markers_broad", "marker_
 # marker_stats
 dim(marker_stats)
 
-
-markers_top <- marker_stats |> 
+markers_mean_ratio_top25 <- marker_stats |> 
   filter(rank_ratio <= 25) |>
   mutate(in_bulk = gene %in% rowData(rse_gene)$ensemblID)
 
-markers_top |>
+markers_mean_ratio_top25 |>
   filter(in_bulk) |>
   dplyr::count(cellType.target)
 
@@ -49,14 +48,21 @@ markers_top |>
 # 6 Excit              23
 # 7 Inhib              20
 
-write.csv(markers_top, file = here("processed-data","08_bulk_deconvolution", "markers_top25.csv"), row.names = FALSE)
+write.csv(markers_mean_ratio_top25, file = here("processed-data","08_bulk_deconvolution", "markers_mean_ratio_top25.csv"), row.names = FALSE)
 
-markers_top25 <- marker_stats |> 
+markers_mean_ratio_top25 <- marker_stats |> 
   dplyr::filter(gene %in% common_genes, rank_ratio <= 25) |>
   dplyr::pull(gene)
 
-cat(markers_top25, sep = "\n", file = here("processed-data","08_bulk_deconvolution", "markers_top25.txt"))
-save(markers_top25, file = here("processed-data","08_bulk_deconvolution", "markers_top25.Rdata"))
+cat(markers_mean_ratio_top25, sep = "\n", file = here("processed-data","08_bulk_deconvolution", "markers_mean_ratio_top25.txt"))
+save(markers_mean_ratio_top25, file = here("processed-data","08_bulk_deconvolution", "markers_mean_ratio_top25.Rdata"))
+
+## 1vALL top25
+markers_1vALL_top25 <- marker_stats |> 
+  dplyr::filter(gene %in% common_genes, rank_marker <= 25) |>
+  dplyr::pull(gene)
+
+cat(markers_1vALL_top25, sep = "\n", file = here("processed-data","08_bulk_deconvolution", "markers_1vALL_top25.txt"))
 
 # slurmjobs::job_single('00_pull_markerse', create_shell = TRUE, memory = '25G', command = "Rscript 00_pull_markers.R")
 
