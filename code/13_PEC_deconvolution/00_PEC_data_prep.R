045e62a33c27b5e1411ff540bbe81cab3d971918
Louise Huuki <lahuuki@gmail.com>
2024-02-14 12:39:32 -0500

    Think about PEC datasets

---
+++ code/13_PEC_deconvolution/00_PEC_data_prep.R
@@ -0,0 +1,13 @@
+
+# pec_data = list(CMC = c(33792, 456460),
+#                 DevBrain = c(33201, 102936),
+#                 IsoHuB = c(33339, 29675),
+#                 LIBD = c(33783, 52214),
+#                 MultiomeBrain = c(33818, 141277),
+#                 PTSDBrainomics = c(33877, 198572),
+#                 SZBDMulti = c(34361, 603209),
+#                 `UCLA-ASD` = c(34180, 448524))
+# 
+# n_cell <- map_int(pec_data,~pluck(.x, 2))
+# 
+# sort(n_cell)
