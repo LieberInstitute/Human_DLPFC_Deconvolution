779dc13315720e3fe022133cc73e1c82572bcd92
Louise Huuki <lahuuki@gmail.com>
2024-02-21 15:54:58 -0500

    Up mem on CIBERSORT FULL run

--- code/08_bulk_deconvolution/07_deconvolution_CIBERSORTx_FULL.sh
+++ code/08_bulk_deconvolution/07_deconvolution_CIBERSORTx_FULL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -p shared
-#SBATCH --mem=50G
+#SBATCH --mem=100G
 #SBATCH --job-name=07_deconvolution_CIBERSORTx_FULL
 #SBATCH -c 1
 #SBATCH -o logs/07_deconvolution_CIBERSORTx_FULL.txt
