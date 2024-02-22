033622991f965df4f712f0029918aef551520ffe
Louise Huuki <lahuuki@gmail.com>
2024-02-22 14:43:48 -0500

    find markers for PEC data

--- code/13_PEC_deconvolution/01_find_markers_PEC.R
+++ code/13_PEC_deconvolution/01_find_markers_PEC.R
@@ -5,33 +5,29 @@ library("here")
 library("sessioninfo")
 
 ## load our DLPFC data
-load(here("processed-data", "sce", "sce_DLPFC.Rdata"), verbose = TRUE)
+load(here("processed-data", "13_PEC_deconvolution", "sce_PTSDBrainomics.Rdata"), verbose = TRUE)
 
-## drop Ambiguous nuc
-sce <- sce[, sce$cellType_broad_hc != "Ambiguous"]
-sce$cellType_broad_hc <- droplevels(sce$cellType_broad_hc)
+## drop cell types 
+sce <- sce[, sce$cellType_broad != "drop"]
+sce$cellType_broad <- droplevels(sce$cellType_broad)
 
-table(sce$cellType_broad_hc)
-# Astro EndoMural     Micro     Oligo       OPC     Excit     Inhib
-# 3979      2157      1601     10894      1940     24809     11067
-
-# cell_type_colors <- metadata(sce)$cell_type_colors_broad[levels(sce$cellType_broad_hc)]
+table(sce$cellType_broad)
+# Astro EndoMural     Excit     Inhib     Micro     Oligo       OPC 
+# 17605       985     86129     36642      9419     35866     10498 
 
 #### Run Marker Finding ####
-
 rowData(sce)$Symbol <- rowData(sce)$gene_name
-rownames(sce) <- rowData(sce)$gene_id
 
 message(Sys.time(), " Find Mean Ratio Genes")
-mean_ratio <- get_mean_ratio2(sce, cellType_col = "cellType_broad_hc", add_symbol = TRUE)
+mean_ratio <- get_mean_ratio2(sce, cellType_col = "cellType_broad", add_symbol = TRUE)
 
 message(Sys.time(), " Find 1vAll genes")
-markers_1vALL <- findMarkers_1vAll(sce, cellType_col = "cellType_broad_hc", mod = "~BrNum")
+markers_1vALL <- findMarkers_1vAll(sce, cellType_col = "cellType_broad", mod = "~BrNum")
 
 marker_stats <- mean_ratio |> left_join(markers_1vALL)
 
 message(Sys.time(), " Save marker gene data")
-save(marker_stats, file = here("processed-data", "06_marker_genes", "03_find_markers_broad", "marker_stats_broad.Rdata"))
+save(marker_stats, file = here("processed-data", "13_PEC_deconvolution", "01_find_markers_PEC", "PEC_marker_stats_broad.Rdata"))
 
 # sgejobs::job_single('03_find_markers_broad', create_shell = TRUE, queue= 'bluejay', memory = '25G', command = "Rscript 03_find_markers_broad.R")
 ## Reproducibility information
