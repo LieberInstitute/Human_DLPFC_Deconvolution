4ed6dec547a821d6ec0a443a7de141cafd8d55eb
Louise Huuki <lahuuki@gmail.com>
2024-02-23 15:01:36 -0500

    Bug removing cell types

--- code/12_tran_deconvolution/00_data_prep_Tran.R
+++ code/12_tran_deconvolution/00_data_prep_Tran.R
@@ -13,14 +13,25 @@ url <- paste0(
 local_data <- BiocFileCache::bfcrpath(url, x = bfc)
 # load sce.dlpfc.tran
 load(local_data, verbose = TRUE)
+table(sce.dlpfc.tran$cellType)
+# Astro    Excit_A    Excit_B    Excit_C    Excit_D    Excit_E    Excit_F    Inhib_A    Inhib_B    Inhib_C 
+# 782        529        773        524        132        187        243        333        454        365 
+# Inhib_D    Inhib_E    Inhib_F Macrophage      Micro      Mural      Oligo        OPC      Tcell 
+# 413          7          8         10        388         18       5455        572          9
 
 sce.dlpfc.tran$cellType_broad <- gsub("_[A-Z]","",sce.dlpfc.tran$cellType)
 sce.dlpfc.tran$cellType_broad <- gsub("Mural", "EndoMural", sce.dlpfc.tran$cellType_broad)
+exp_cell_types <- c("Astro", "EndoMural", "Micro", "Oligo","OPC", "Excit", "Inhib") ## cell types considered in this experiment
 
-exp_cell_types <- c("Astro", "EndoMural", "Micro", "Oligo","OPC", "Excit", "Inhib")
-sce.dlpfc.tran <- sce.dlpfc.tran[sce.dlpfc.tran$cellType_broad %in% exp_cell_types]
+table(sce.dlpfc.tran$cellType_broad %in% exp_cell_types,
+      sce.dlpfc.tran$cellType_broad)
+# Astro EndoMural Excit Inhib Macrophage Micro Oligo  OPC Tcell
+# FALSE     0         0     0     0         10     0     0    0     9
+# TRUE    782        18  2388  1580          0   388  5455  572     0
 
+sce.dlpfc.tran <- sce.dlpfc.tran[,sce.dlpfc.tran$cellType_broad %in% exp_cell_types] ## drop Micro & Tcell
 sce.dlpfc.tran$cellType_broad <- factor(sce.dlpfc.tran$cellType_broad, levels = exp_cell_types)
+any(is.na(sce.dlpfc.tran$cellType_broad))
 
 table(sce.dlpfc.tran$cellType_broad)
 # Astro EndoMural     Micro     Oligo       OPC     Excit     Inhib 
