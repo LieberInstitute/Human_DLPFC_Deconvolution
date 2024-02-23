4730fcc1a7d999304a9b2137afc589bd0cbbf775
Louise Huuki <lahuuki@gmail.com>
2024-02-23 16:37:34 -0500

    Adapt Bisque run for Tran data

--- code/12_tran_deconvolution/02_deconvolution_Bisque.R
+++ code/12_tran_deconvolution/02_deconvolution_Bisque.R
@@ -18,7 +18,7 @@ if(marker_label == "FULL"){
 }
 
 #### data output folder ####
-data_dir <- here("processed-data","08_bulk_deconvolution", "01_deconvolution_Bisque")
+data_dir <- here("processed-data","12_tran_deconvolution", "01_deconvolution_Bisque")
 if(!dir.exists(data_dir)) dir.create(data_dir)
 
 #### load data ####
@@ -30,17 +30,13 @@ dim(rse_gene)
 rownames(rse_gene) <- rowData(rse_gene)$ensemblID
 
 ## sce data
-load(here("processed-data", "sce", "sce_DLPFC.Rdata"), verbose = TRUE)
-
-sce <- sce[,sce$cellType_broad_hc != "Ambiguous"]
-sce$cellType_broad_hc <- droplevels(sce$cellType_broad_hc)
-
-rownames(sce) <- rowData(sce)$gene_id
+load(here("processed-data", "12_tran_deconvolution", "sce.dlpfc.tran.Rdata"), verbose = TRUE)
+table(sce$cellType_broad)
 
 ## find common genes
 common_genes <- intersect(rowData(sce)$gene_id, rowData(rse_gene)$ensemblID)
 length(common_genes)
-# [1] 17804
+
 
 if(marker_label == "FULL"){
   markers <- common_genes
@@ -61,7 +57,7 @@ exp_set_bulk <- ExpressionSet(assayData = assays(rse_gene)$counts[markers,],
 
 exp_set_sce <- ExpressionSet(assayData = as.matrix(assays(sce)$counts[markers,]),
                              phenoData=AnnotatedDataFrame(
-                               as.data.frame(colData(sce)[,c("key","Sample","BrNum", "cellType_broad_hc", "cellType_hc")])))
+                               as.data.frame(colData(sce)[,c("donor", "cellType_broad")])))
 
 ### run Bisque ####
 exp_set_sce_temp <- exp_set_sce[markers,]
@@ -73,11 +69,11 @@ exp_set_sce_temp <- exp_set_sce_temp[,zero_cell_filter]
 message(Sys.time(), " - Bisque deconvolution")
 est_prop_bisque <- ReferenceBasedDecomposition(bulk.eset = exp_set_bulk[markers,],
                                                sc.eset = exp_set_sce_temp,
-                                               cell.types = "cellType_broad_hc",
-                                               subject.names = "Sample",
+                                               cell.types = "cellType_broad",
+                                               subject.names = "donor",
                                                use.overlap = FALSE)
 
-save(est_prop_bisque, file = here(data_dir, paste0("est_prop_bisque-",marker_label,".Rdata")))
+save(est_prop_bisque, file = here(data_dir, paste0("Tran_est_prop_bisque-",marker_label,".Rdata")))
 
 # slurmjobs::job_single('01_deconvolution_Bisque_FULL', create_shell = TRUE, memory = '100G', command = "Rscript 01_deconvolution_Bisque.R FULL")
 
