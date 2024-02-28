479478a8137eea6f96e7832ada2aebaf9f4866af
Nick-Eagles <nick.eagles@libd.org>
2024-02-28 14:38:22 -0500

    Read in cell-size info from HALO; use it to produce a data frame of cell sizes dependent on the array task for use in the 'cell_size' parameter to MuSiC

--- code/08_bulk_deconvolution/02_deconvolution_MuSiC_cell_size.R
+++ code/08_bulk_deconvolution/02_deconvolution_MuSiC_cell_size.R
@@ -1,16 +1,29 @@
+library("SingleCellExperiment")
+library("MuSiC")
+library("here")
+library("sessioninfo")
+library("HDF5Array")
+library("tidyverse")
+
+#   Later controls the argument to 'cell_size' parameter for music_prop
+cell_size_opt = c("nuc_area", "akt3", "nuc_area_akt3")[
+    as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+]
+
 marker_label <- 'MeanRatio_top25'
 marker_file <- here(
     'processed-data', '08_bulk_deconvolution', 'markers_MeanRatio_top25.txt'
 )
 sce_path = here("processed-data", "sce", "sce_DLPFC.Rdata")
 bulk_path = here("processed-data","rse", "rse_gene.Rdata")
+halo_path = here("processed-data", "03_HALO", "halo_all.Rdata")
 out_path <- here(
     "processed-data", "08_bulk_deconvolution", "02_deconvolution_MuSiC",
     sprintf("est_prop_bisque_%s_%s_random_subsets.csv", marker_label, n_runs)
 )
 
-
-message("Using ", marker_label," marker genes from:", marker_file)
+message("Using ", marker_label," marker genes from: ", marker_file)
+message(sprintf("Using cell size option '%s'.", cell_size_opt))
 
 ################################################################################
 #   Load bulk data, single-cell data, and top-25 mean-ratio markers
@@ -27,6 +40,10 @@ load(sce_path, verbose = TRUE)
 rownames(sce) <- rowData(sce)$gene_id
 colnames(sce) = sce$key
 
+#   Drop Ambiguous cell type
+sce <- sce[,sce$cellType_broad_hc != "Ambiguous"]
+sce$cellType_broad_hc <- droplevels(sce$cellType_broad_hc)
+
 ## find common genes
 common_genes <- intersect(rowData(sce)$gene_id, rowData(rse_gene)$ensemblID)
 length(common_genes)
@@ -42,6 +59,50 @@ if(!all(markers %in% common_genes)) {
 }
 markers <- intersect(markers, common_genes)
 
+load(halo_path, verbose = TRUE)
+
+################################################################################
+#   Load HALO data and produce a table of "cell sizes" for each cell type
+################################################################################
+
+#   Keep only cell types that match the single-cell data; treat "OligoOPC"
+#   as if it's just oligos (later we'll use define OPCs to have the same cell
+#   sizes as oligos, since this category was merged in the HALO data and not
+#   recoverable into distinct categories)
+halo_all = halo_all |>
+    mutate(cell_type = ifelse(cell_type == "OligoOPC", "Oligo", cell_type)) |>
+    filter(cell_type != "Other")
+stopifnot(all(halo_all$cell_type %in% unique(sce$cellType_broad_hc)))
+
+#   For each cell type, compute an average "cell size" based on some combination
+#   of nuclear area and ATK3 puncta
+if (cell_size_opt == "nuc_area") {
+    cell_size_df = halo_all |>
+        group_by(cell_type) |>
+        summarize(cell_size = mean(Nucleus_Area))
+} else if (cell_size_opt == "akt3") {
+    cell_size_df = halo_all |>
+        group_by(cell_type) |>
+        summarize(cell_size = mean(DAPI_AKT3))
+} else if (cell_size_opt == "nuc_area_akt3") {
+    cell_size_df = halo_all |>
+        group_by(cell_type) |>
+        summarize(cell_size = mean(Nucleus_Area * DAPI_AKT3))
+} else {
+    stop("Invalid cell-size option.")
+}
+
+#   Add a row for OPC that matches the row for Oligo
+cell_size_df = rbind(
+    cell_size_df,
+    tibble(
+        cell_type = 'OPC',
+        cell_size = cell_size_df |>
+            filter(cell_type == "Oligo") |>
+            pull(cell_size)
+    )
+) |> as.data.frame()
+
 ################################################################################
 #   Run MuSiC and save
 ################################################################################
@@ -53,7 +114,7 @@ est_prop_music <- music_prop(
     markers = markers,
     clusters = "cellType_broad_hc",
     samples = "Sample",
-    cell_size = NULL
+    cell_size = cell_size_df
 )
 
 save(est_prop_music, file = here(data_dir,paste0("est_prop_music-", marker_label, ".Rdata")))
