267040fa3fc0229c8dbaa372c9bcd149a393cb9b
Louise Huuki <lahuuki@gmail.com>
2024-02-28 17:36:45 -0500

    update input plots, plot barplot of input stats

--- code/12_tran_deconvolution/05_deconvo_input_plots.R
+++ code/12_tran_deconvolution/05_deconvo_input_plots.R
@@ -5,6 +5,7 @@ library("DeconvoBuddies")
 library("here")
 library("viridis")
 library("GGally")
+library("patchwork")
 
 plot_dir <- here("plots", "12_tran_deconvolution", "05_deconvo_input_plots")
 if (!dir.exists(plot_dir)) dir.create(plot_dir, recursive = TRUE)
@@ -19,30 +20,119 @@ load(here("processed-data","00_data_prep","library_combo_shapes.Rdata"), verbose
 # library_combo_shapes
 # library_combo_shapes2
 
+#### input qualities ####
+ct_prop_paired <- read_csv(here("processed-data", "03_HALO", "08_explore_proportions", "snRNA_cell_type_proportions_opc.csv")) |>
+  mutate(dataset = "paired", 
+         donor = gsub("_\\w+","", Sample),
+         dx = "Control") |>
+  select(dataset, Sample, donor, dx, cell_type = cellType_broad_hc , n_cell_sn, prop_sn)
+
+ct_prop_tran <- read.csv(here("processed-data", "12_tran_deconvolution", "tran_ct_prop.csv"), row.names = 1) |>
+  as_tibble() |>
+  mutate(dataset = "Tran", 
+         Sample = donor,
+         dx = "Control")  |>
+  select(dataset, Sample, donor, dx, cell_type = cellType_broad, n_cell_sn = n, prop_sn = prop)
+
+ct_prop_PEC <- read.csv(here("processed-data", "13_PEC_deconvolution", "PEC_ct_prop.csv"), row.names = 1) |>
+  as_tibble() |>
+  mutate(dataset = "PEC", Sample = donor) |>
+  select(dataset, Sample, donor, dx, cell_type = cellType_broad, n_cell_sn = n, prop_sn = prop)
+
+ct_prop <- bind_rows(ct_prop_paired, ct_prop_tran) |>
+  bind_rows(ct_prop_PEC) |>
+  mutate(dx = ifelse(dx != "Control", "Case", "Control"),
+         cell_type = factor(cell_type, levels = names(cell_type_colors_broad)))
+
+dataset_donor_n <- ct_prop |>
+  group_by(dataset, donor, dx) |>
+  count() |>
+  group_by(dataset,  dx) |>
+  count()
+
+
+dataset_prop <- ct_prop |>
+  group_by(dataset, cell_type) |>
+  summarize(n_cells = sum(n_cell_sn)) |>
+  group_by(dataset) |>
+  mutate(prop = n_cells/sum(n_cells))
+
+## bar plots 
+donor_n_col <- dataset_donor_n |>
+  ggplot(aes(x = dataset, y = n, fill = dx)) +
+  geom_col() +
+  geom_text(aes(label = n),
+            position = position_stack(vjust = .5)) +
+  theme_bw()  +
+  labs(y = "Number Donors", x = NULL)
+
+dataset_cell_bar <- dataset_prop |>
+  ggplot(aes(x = dataset, y = n_cells, fill = cell_type)) +
+  geom_col() +
+  # geom_text(aes(label = n_cells),
+  #           position = position_stack(vjust = .5)) +
+  theme_bw() +
+  scale_fill_manual(values = cell_type_colors_broad) +
+  labs(y = "Number of Nuclei", x = NULL)
+
+dataset_prop_bar <- dataset_prop |>
+  ggplot(aes(x = dataset, y = prop, fill = cell_type)) +
+  geom_col() +
+  geom_text(aes(label = ifelse(prop > .05, round(prop, 3), "")),
+            position = position_stack(vjust = .5),
+            size = 2.5) +
+  theme_bw() +
+  scale_fill_manual(values = cell_type_colors_broad) +
+  labs(y = "Cell Type Proportion")
+
+data_set_cols <- donor_n_col / dataset_cell_bar /dataset_prop_bar
+ggsave(data_set_cols, filename = here(plot_dir, "dataset_bar_plots.png"), width = 5, height = 7)  
+ggsave(data_set_cols, filename = here(plot_dir, "dataset_bar_plots.pdf"), width = 5, height = 7)  
+
 #### load data - combine input datasets ####
 load(here("processed-data", "08_bulk_deconvolution", "03_get_est_prop","prop_long.Rdata"), verbose = TRUE)
 prop_long_paired <- prop_long |>
   filter(method %in% c("Bisque", "hspe"),
          marker == "MeanRatio_top25") |>
   mutate(input = "paired")
+
+prop_long_opc_paired <- prop_long_opc |>
+  filter(method %in% c("Bisque", "hspe"),
+         marker == "MeanRatio_top25") |>
+  mutate(input = "paired")
   
 load(here("processed-data", "12_tran_deconvolution", "04_get_est_prop","Tran_prop_long.Rdata"), verbose = TRUE)
-prop_long_tran <- prop_long
+prop_long_tran <- prop_long |>
+  mutate(input = "Tran")
+
+prop_long_opc_tran <- prop_long_opc |>
+  mutate(input = "Tran")
 
 load(here("processed-data", "13_PEC_deconvolution", "04_get_est_prop","PEC_prop_long.Rdata"), verbose = TRUE)
-prop_long <- bind_rows(prop_long, prop_long_tran) |>
+prop_long <- bind_rows(prop_long |>
+                         mutate(input = "PEC"), 
+                       prop_long_tran) |>
   bind_rows(prop_long_paired) |>
   mutate(factor(input, levels = c("paired", "Tran", "PEC")))
 
+prop_long_opc <- bind_rows(prop_long_opc |>
+                         mutate(input = "PEC"), 
+                       prop_long_opc_tran) |>
+  bind_rows(prop_long_opc_paired) |>
+  mutate(factor(input, levels = c("paired", "Tran", "PEC")))
+
 prop_long |> filter(is.na(RNAscope_prop)) |> count(input, method)
-# input method     n
-# <chr> <chr>  <int>
-# 1 PEC   Bisque   434
-# 2 PEC   hspe     434
-# 3 Tran  Bisque   434
-# 4 Tran  hspe     434
-# 5 paired Bisque   434
-# 6 paired hspe     434
+# input  method     n
+# <chr>  <chr>  <int>
+# 1 PEC    Bisque   261
+# 2 PEC    hspe     261
+# 3 Tran   Bisque   261
+# 4 Tran   hspe     261
+# 5 paired Bisque   261
+# 6 paired hspe     261
+
+prop_long |> count(cell_type)
+prop_long_opc |> count(cell_type)
 
 #### correlation ####
 (cor_check <- prop_long |>
@@ -53,14 +143,14 @@ prop_long |> filter(is.na(RNAscope_prop)) |> count(input, method)
    mutate(cor_anno = sprintf("cor:%.3f\nrmse:%.3f", round(cor,3), round(rmse,3)))|>
    arrange(cor))
 
-# input method   cor  rmse cor_anno               
-# <chr> <chr>  <dbl> <dbl> <chr>                  
-# 1 Tran   Bisque 0.102 0.196 "cor:0.102\nrmse:0.196"
-# 2 PEC    hspe   0.296 0.120 "cor:0.296\nrmse:0.120"
-# 3 Tran   hspe   0.503 0.143 "cor:0.503\nrmse:0.143"
-# 4 paired Bisque 0.508 0.148 "cor:0.508\nrmse:0.148"
-# 5 paired hspe   0.513 0.151 "cor:0.513\nrmse:0.151"
-# 6 PEC    Bisque 0.534 0.138 "cor:0.534\nrmse:0.138"
+# input  method   cor  rmse cor_anno               
+# <chr>  <chr>  <dbl> <dbl> <chr>                  
+#   1 Tran   Bisque 0.176 0.197 "cor:0.176\nrmse:0.197"
+# 2 PEC    hspe   0.238 0.127 "cor:0.238\nrmse:0.127"
+# 3 Tran   hspe   0.496 0.137 "cor:0.496\nrmse:0.137"
+# 4 paired hspe   0.532 0.143 "cor:0.532\nrmse:0.143"
+# 5 paired Bisque 0.538 0.141 "cor:0.538\nrmse:0.141"
+# 6 PEC    Bisque 0.563 0.135 "cor:0.563\nrmse:0.135"
 
 (cor_check_library <- prop_long |>
     filter(!is.na(RNAscope_prop)) |>
@@ -72,16 +162,16 @@ prop_long |> filter(is.na(RNAscope_prop)) |> count(input, method)
 )
 # method input  library_type rna_extract library_combo        cor  rmse cor_anno               
 # <chr>  <chr>  <chr>        <chr>       <chr>              <dbl> <dbl> <chr>                  
-#   1 Bisque PEC    polyA        Cyto        polyA_Cyto         0.675 0.109 "cor:0.675\nrmse:0.109"
-# 2 Bisque paired polyA        Cyto        polyA_Cyto         0.672 0.131 "cor:0.672\nrmse:0.131"
-# 3 hspe   Tran   polyA        Cyto        polyA_Cyto         0.607 0.135 "cor:0.607\nrmse:0.135"
-# 4 hspe   paired polyA        Cyto        polyA_Cyto         0.598 0.144 "cor:0.598\nrmse:0.144"
-# 5 Bisque paired polyA        Nuc         polyA_Nuc          0.560 0.136 "cor:0.560\nrmse:0.136"
-# 6 Bisque PEC    polyA        Nuc         polyA_Nuc          0.552 0.125 "cor:0.552\nrmse:0.125"
-# 7 Bisque PEC    RiboZeroGold Cyto        RiboZeroGold_Cyto  0.524 0.141 "cor:0.524\nrmse:0.141"
-# 8 hspe   paired RiboZeroGold Total       RiboZeroGold_Total 0.522 0.152 "cor:0.522\nrmse:0.152"
-# 9 Bisque PEC    RiboZeroGold Nuc         RiboZeroGold_Nuc   0.519 0.163 "cor:0.519\nrmse:0.163"
-# 10 Bisque PEC    RiboZeroGold Total       RiboZeroGold_Total 0.511 0.145 "cor:0.511\nrmse:0.145"
+# 1 Bisque PEC    polyA        Cyto        polyA_Cyto         0.685 0.110 "cor:0.685\nrmse:0.110"
+# 2 Bisque paired polyA        Cyto        polyA_Cyto         0.683 0.123 "cor:0.683\nrmse:0.123"
+# 3 hspe   paired polyA        Cyto        polyA_Cyto         0.605 0.138 "cor:0.605\nrmse:0.138"
+# 4 Bisque paired polyA        Nuc         polyA_Nuc          0.587 0.127 "cor:0.587\nrmse:0.127"
+# 5 hspe   Tran   polyA        Cyto        polyA_Cyto         0.585 0.132 "cor:0.585\nrmse:0.132"
+# 6 Bisque PEC    polyA        Nuc         polyA_Nuc          0.579 0.121 "cor:0.579\nrmse:0.121"
+# 7 Bisque PEC    RiboZeroGold Nuc         RiboZeroGold_Nuc   0.557 0.157 "cor:0.557\nrmse:0.157"
+# 8 Bisque PEC    RiboZeroGold Cyto        RiboZeroGold_Cyto  0.556 0.139 "cor:0.556\nrmse:0.139"
+# 9 hspe   paired RiboZeroGold Total       RiboZeroGold_Total 0.549 0.143 "cor:0.549\nrmse:0.143"
+# 10 Bisque PEC    RiboZeroGold Total       RiboZeroGold_Total 0.547 0.141 "cor:0.547\nrmse:0.141"
 
 ## cor check line/rank plot
 cor_rmse_line <- cor_check_library |>
@@ -109,7 +199,7 @@ ggsave(cor_rmse_scater_method, filename = here(plot_dir, "cor_rmse_scater_method
 ggsave(cor_rmse_scater_method, filename = here(plot_dir, "cor_rmse_scater_method_input.pdf"), width = 7, height = 4)
 
 ## bar plot
-prop_bar_SAMPLE_facet <- prop_long |> 
+prop_bar_SAMPLE_facet <- prop_long_opc |> 
   mutate(Sample = gsub("_","\n", Sample)) |>
   ggplot(aes(x = library_combo, y = prop, fill = cell_type)) +
   geom_bar(stat = "identity") +
