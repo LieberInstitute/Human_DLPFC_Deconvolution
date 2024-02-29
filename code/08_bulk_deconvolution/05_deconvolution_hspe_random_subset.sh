a45a6838d968e737df21713caf226848d1fd72e5
lahuuki <lahuuki@gmail.com>
2024-02-29 09:55:15 -0500

    Add hspe subset sh file Co-authored-by: Nick Eagles <nickeagles77@gmail.com>

---
+++ code/08_bulk_deconvolution/05_deconvolution_hspe_random_subset.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH -p shared
+#SBATCH --mem=3G
+#SBATCH --job-name=05_deconvolution_hspe_random_subset
+#SBATCH -c 1
+#SBATCH -o logs/05_deconvolution_hspe_random_subset_%a.txt
+#SBATCH -e logs/05_deconvolution_hspe_random_subset_%a.txt
+#SBATCH --array=6-1000%15
+
+set -e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## Load the R module
+module load conda_R/4.3.x
+
+## List current modules for reproducibility
+module list
+
+Rscript 05_deconvolution_hspe_random_subset.R
+
+echo "**** Job ends ****"
+date
