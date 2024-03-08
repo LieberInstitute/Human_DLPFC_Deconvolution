6796bf22d29919a0a6a09f84fb275ba3d5a9cf42
Louise Huuki <lahuuki@gmail.com>
2024-03-08 16:23:10 -0500

    Export marker status w/ over2, mad3 cols

--- code/06_marker_genes/07_export_marker_genes.R
+++ code/06_marker_genes/07_export_marker_genes.R
@@ -12,18 +12,28 @@ load(here("processed-data","rse", "rse_gene.Rdata"), verbose = TRUE)
 
 rd <- as.data.frame(rowData(rse_gene))
 
-marker_stats <- marker_stats |>
-  mutate(in_bulk = gene %in% rd$ensemblID)
+marker_stats2 <- marker_stats |>
+  group_by(cellType.target) |>
+  mutate(in_bulk = gene %in% rd$ensemblID,
+         MeanRatio_top25 = in_bulk & rank_ratio <= 25,
+         MeanRatio_over2 = in_bulk & ratio > 2,
+         MeanRatio_MAD3 = in_bulk & (ratio > median(ratio) + 3*mad(ratio)),
+         `1vALL_top25` = in_bulk & rank_marker <= 25
+         )
 
-marker_stats |> count(cellType.target, in_bulk)
+marker_stats2 |> 
+  ungroup()|> 
+  summarise(MeanRatio_top25 = sum(MeanRatio_top25),
+            MeanRatio_over2 = sum(MeanRatio_over2),
+            MeanRatio_MAD3 = sum(MeanRatio_MAD3),
+            `1vALL_top25` = sum(`1vALL_top25`),
+            )
 
-
-marker_genes_top25 <- marker_stats |>
+marker_genes_top25 <- marker_stats2 |>
   group_by(cellType.target) |>
   # filter(in_bulk, rank_ratio <=25)
-  filter(in_bulk) |>
-  arrange(-ratio) |>
-  slice(1:25)
+  filter(MeanRatio_top25) |>
+  arrange(-ratio)
 
 marker_genes_top25 |> count(cellType.target)
 ## top 25 that overlap w/ rse_genes
@@ -37,6 +47,7 @@ marker_genes_top25 |> count(cellType.target)
 # 6 Excit              23
 # 7 Inhib              20
 
+## for MR top25, whats the lowest MR?
 marker_genes_top25 |>
   arrange(ratio) |>
   slice(1) |>
@@ -45,13 +56,13 @@ marker_genes_top25 |>
 
 # cellType.target gene            Symbol     rank_ratio ratio logFC
 # <fct>           <chr>           <chr>           <int> <dbl> <dbl>
-# 1 OPC             ENSG00000103489 XYLT1              33  2.80  9.97
-# 2 Inhib           ENSG00000132170 PPARG              30  3.00  1.79
-# 3 Astro           ENSG00000286288 AL109809.5         26  3.90  1.59
-# 4 Excit           ENSG00000106089 STX1A              27  4.36  1.97
-# 5 EndoMural       ENSG00000137962 ARHGAP29           26  5.90  3.71
-# 6 Oligo           ENSG00000286749 AC008571.2         25  7.08  3.93
-# 7 Micro           ENSG00000109794 FAM149A            35  9.36  1.07
+#   1 Inhib           ENSG00000120049 KCNIP2             25  3.39 0.963
+# 2 OPC             ENSG00000231871 IPO9-AS1           25  3.75 1.12 
+# 3 Astro           ENSG00000143603 KCNN3              25  4.10 1.25 
+# 4 Excit           ENSG00000119125 GDA                25  4.45 3.81 
+# 5 EndoMural       ENSG00000176641 RNF152             25  5.99 1.99 
+# 6 Oligo           ENSG00000286749 AC008571.2         25  7.08 3.93 
+# 7 Micro           ENSG00000169313 P2RY12             25 14.9  2.21 
 
 #### export ####
 
@@ -62,3 +73,6 @@ marker_genes_ensembl <- map(splitit(marker_genes_top25$cellType.target), ~marker
 
 save(marker_genes_top25, marker_genes_top25_simple, marker_genes_ensembl, file = here("processed-data","06_marker_genes","marker_genes_top25.Rdata"))
 
+#### Write csv ####
+
+write_csv(marker_stats2, here("processed-data", "06_marker_genes", "marker_stats_broad.csv"))
