1b0022b68bccefd893dd9b324814716d385181cc
Nick-Eagles <nick.eagles@libd.org>
2024-03-08 13:44:44 -0500

    Add a script for uploading bulkRNA FASTQs to SRA

---
+++ code/11_raw_data_upload/05_upload_fastq.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH -p transfer
+#SBATCH -c 1
+#SBATCH --mem=3G
+#SBATCH -t 3-00:00:00
+#SBATCH --job-name=05_upload_fastq
+#SBATCH -o ../../processed-data/11_raw_data_upload/05_upload_fastq.log
+#SBATCH -e ../../processed-data/11_raw_data_upload/05_upload_fastq.log
+
+set -e
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+
+fastq_dir=$(git rev-parse --show-toplevel)/raw-data/bulkRNA/flat_dir
+ascp \
+    -i ~/aspera.openssh \
+    -QT \
+    -l100m \
+    -k1 \
+    -d \
+    $fastq_dir \
+    $ASP_DEST_DIR # privately sourced environment variable for destination dir
+
+echo "**** Job ends ****"
+date
