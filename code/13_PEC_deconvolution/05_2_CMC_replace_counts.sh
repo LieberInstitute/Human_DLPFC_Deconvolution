cb8d2cd4475a4a874ccbef175403fe80146f5820
Nick-Eagles <nick.eagles@libd.org>
2024-07-26 16:08:49 -0400

    Add a shell script to run the script to add raw counts to the CMC SCE

---
+++ code/13_PEC_deconvolution/05_2_CMC_replace_counts.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH -p shared
+#SBATCH --mem=30G
+#SBATCH --job-name=05_2_CMC_replace_counts
+#SBATCH -c 1
+#SBATCH -t 1-00:00:00
+#SBATCH -o logs/05_2_CMC_replace_counts.txt
+#SBATCH -e logs/05_2_CMC_replace_counts.txt
+
+set -e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## Load the R module
+module load conda_R/4.3.x
+
+## List current modules for reproducibility
+module list
+
+Rscript 05_2_CMC_replace_counts.R
+
+echo "**** Job ends ****"
+date
+
+## This script was made using slurmjobs version 1.2.2
+## available from http://research.libd.org/slurmjobs/
