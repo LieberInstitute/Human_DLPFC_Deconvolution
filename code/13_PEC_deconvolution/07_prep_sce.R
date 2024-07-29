a0eddfba5da842ecb336cdaa4d3371fa0ce7924c
Nick-Eagles <nick.eagles@libd.org>
2024-07-29 15:29:49 -0400

    Add broad cell type resolution and drop certain cell types

--- code/13_PEC_deconvolution/07_prep_sce.R
+++ code/13_PEC_deconvolution/07_prep_sce.R
@@ -16,9 +16,26 @@ sce_in_path = here("processed-data", "13_PEC_deconvolution", "sce_CMC.rds")
 sce_out_path = here(
     "processed-data", "13_PEC_deconvolution", "sce_CMC_subset.rds"
 )
+ct_table_path = here(
+    "processed-data", "13_PEC_deconvolution", "PEC_cell_types.csv"
+)
 n_markers_per_type = 25
 
 sce = readRDS(sce_in_path)
+
+#   Remove logcounts, which are large and no longer needed
+assays(sce)$logcounts = NULL
+gc()
+
+#   Add cell type at broad resolution. Drop certain broad cell types as
+#   consistent with the other PEC analysis
+ct_table = read_csv(ct_table_path, show_col_types = FALSE)
+stopifnot(all(sce$subclass %in% ct_table$subclass))
+sce$cell_type_broad = ct_table$cellType_broad[
+    match(sce$subclass, ct_table$subclass)
+]
+sce = sce[, sce$cell_type_broad != "drop"]
+
 marker_stats = read_csv(marker_stats_path, show_col_types = FALSE)
 
 #   Take up to 25 markers per cell type, provided all ratios exceed 1
@@ -29,11 +46,8 @@ filtered_stats = marker_stats |>
     slice_head(n = n_markers_per_type) |>
     ungroup()
 
-#   Subset to markers, cell types for which markers exist, and make counts dense
-sce = sce[
-    filtered_stats$gene,
-    sce$subclass %in% unique(filtered_stats$cellType.target)
-]
+#   Subset to markers and make counts dense
+sce = sce[filtered_stats$gene,]
 assays(sce) = list(counts = as.matrix(assays(sce)$counts))
 saveRDS(sce, sce_out_path)
 
