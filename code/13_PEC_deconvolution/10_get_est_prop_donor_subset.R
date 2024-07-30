cd119c933ced29bc415e9850dd34929f79fabfed
Louise Huuki <lahuuki@gmail.com>
2024-07-30 16:38:43 -0400

    Plot dononr subset data

--- code/13_PEC_deconvolution/10_get_est_prop_donor_subset.R
+++ code/13_PEC_deconvolution/10_get_est_prop_donor_subset.R
@@ -39,7 +39,7 @@ fn_hspe <- list.files(here("processed-data", "13_PEC_deconvolution", "hspe_donor
 names(fn_hspe) <- gsub("est_prop_(.*?)_random_subsets.csv","\\1",basename(fn_hspe))
 length(fn_hspe)
 
-est_prop_hspe <- map_dfr(fn_hspe, ~read.csv(.x) |> mutate(method = "hspe"))
+est_prop_hspe <- map_dfr(fn_hspe, ~read.csv(.x))
 
 est_prop_hspe |> count(num_donors)
 # num_donors     n
@@ -71,12 +71,15 @@ prop_long_donor_subset_summary_opc <- prop_long_donor_subset |>
   mutate(Sample = paste0(BrNum, "_", tolower(pos)))
 
 ## combine OligoOPC
-prop_long_donor_subset_summary <- prop_long_donor_subset |>
+
+prop_long_donor_OligoOPC <- prop_long_donor_subset |>
   mutate(cell_type = ifelse(cell_type %in% c("OPC", "Oligo"),
                             "OligoOPC", 
                             cell_type))|>
   group_by(SAMPLE_ID, num_donors, subset_run, method, cell_type)|>
-  summarise(prop = sum(prop)) |>
+  summarise(prop = sum(prop))
+
+prop_long_donor_subset_summary <- prop_long_donor_OligoOPC  |>
   group_by(SAMPLE_ID, cell_type, num_donors, method) |> 
   summarise(mean_prop = mean(prop),
             median_prop = median(prop),
@@ -93,6 +96,7 @@ prop_long_donor_subset_summary <- prop_long_donor_subset |>
 
 prop_long_donor_subset_summary |> count(cell_type)
 
+
 ## add data to prop_long_opc
 sn_prop_opc <- read.csv(here("processed-data", "03_HALO", "08_explore_proportions", "snRNA_cell_type_proportions_opc.csv")) |>
   select(Sample, cell_type = cellType_broad_hc, snRNA_prop = prop_sn)
@@ -104,8 +108,24 @@ prop_long_donor_subset_summary_opc <- prop_long_donor_subset_summary_opc |>
          library_combo = paste0(library_type, "_",rna_extract),
          cell_type = factor(cell_type, levels = c("Astro","EndoMural","Micro","Oligo","OPC","Excit","Inhib")))
 
+#### Calc cor + rmse by subsamle ####
+
+prop_long_donor_OligoOPC <- prop_long_donor_OligoOPC |>
+  left_join(halo_prop_simple, relationship = "many-to-many")
+
+cor_check_subset <- prop_long_donor_OligoOPC |>
+  filter(!is.na(RNAscope_prop)) |>
+  group_by(method, num_donors, subset_run) |>
+  summarize(cor = cor(RNAscope_prop, prop),
+            rmse = Metrics::rmse(RNAscope_prop, prop))
+
+
+cor_check_subset |>
+  group_by(method, num_donors) |>
+  summarize(min(cor), max(cor), median(cor), n())
+
 ## export this data
-save(prop_long_donor_subset, prop_long_donor_subset_summary_opc, prop_long_donor_subset_summary, file = here(data_dir, "prop_long_donor_subset.Rdata"))
+save(prop_long_donor_subset, cor_check_subset, prop_long_donor_OligoOPC, prop_long_donor_subset_summary_opc, prop_long_donor_subset_summary, file = here(data_dir, "prop_long_donor_subset.Rdata"))
 
 ## as csv
 write_csv(prop_long_donor_subset_summary, file = here(data_dir, "prop_long_donor_subset_summary.csv"))
