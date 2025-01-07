47ace4c1b2ba211154041d63a05aac5f8f6513d6
Louise Huuki <lahuuki@gmail.com>
2025-01-07 17:10:27 -0500

    use slurmjob to start CIBERSROTx HVG sh file

--- code/08_bulk_deconvolution/16_deconvo_HVG_run.R
+++ code/08_bulk_deconvolution/16_deconvo_HVG_run.R
@@ -5,7 +5,7 @@ library("here")
 
 #### slurmjob setup ####
 
-# hvg_prop <- as.character(seq(10,100, 10))
+hvg_prop <- as.character(seq(10,100, 10))
 # hvg_files <- sprintf("../../processed-data/06_marker_genes/09_HVGs/HVG%d0.txt", seq(1,10))
 # all(file.exists(hvg_files))
 
@@ -14,11 +14,16 @@ library("here")
 #          name = "01_deconvolution_Bisque_HVG",
 #          create_shell = TRUE)
 
+## Use sh file from 01_deconvolution_Bisque_HVG
 ## 2 02_deconvolution_MuSiC
 ## 3 04_deconvolution_DWLS
 ## 4 05_deconvolution_hspe
 ## 5 06_deconvolution_BayesPrism_marker
+
 ## 6 07_deconvolution_CIBERSORTx
+# job_loop(loops = list(HVG=hvg_prop),
+#          name = "07_deconvolution_CIBERSORTx_HVG",
+#          create_shell = TRUE)
 
 #### prep dirs & plotting ####
 
